b4995893c829d822d3ca40a3fbfbc856765ed2cb
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-02-02 10:58:53 +0530

    modified

--- src/RECURSION/PrintOneToHunderd.java
+++ src/RECURSION/PrintOneToHunderd.java
@@ -1,7 +1,10 @@
 package RECURSION;
 public class PrintOneToHunderd {
     public static void main(String[] args) {
-
-
+        System.out.println("welcome");
+        method();
+    }
+    static void method() {
+        System.out.println("welcome");
     }
 }
