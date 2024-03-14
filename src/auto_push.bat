a59913c49f0f3fc0fa65151e9fb49e98ed17547e
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-14 21:30:54 +0530

    modified

--- src/auto_push.bat
+++ src/auto_push.bat
@@ -4,7 +4,7 @@
 git status --porcelain | findstr "." > nul
 if %errorlevel% == 0 (
     git add .
-    git commit -m "Automatic commit"
+    git commit -m "modified"
     git push origin master
 )
 timeout /t 3 > nul
