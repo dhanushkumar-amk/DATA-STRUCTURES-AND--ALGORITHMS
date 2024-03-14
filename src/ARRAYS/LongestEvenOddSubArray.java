286e9f5219095901d88bbf8a1e8145d010ee8d50
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-14 21:38:23 +0530

    modified

--- src/ARRAYS/LongestEvenOddSubArray.java
+++ src/ARRAYS/LongestEvenOddSubArray.java
@@ -3,13 +3,11 @@ public class LongestEvenOddSubArray {
     static void LongestEvenOddSubarray (int [] arr){
         int n= arr.length;
         for (int i = 0; i < n; i++) {
-            System.out.println("Arunkumar");
+
         }
     }
     public static void main(String[] args) {
         int [] arr = {10,12,14,7,8};
         LongestEvenOddSubarray(arr);
-        System.out.println("Arunkumar");
-        System.out.println("arunxx");
     }
 }
