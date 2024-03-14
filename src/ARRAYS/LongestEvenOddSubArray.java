2de1ecb8c8eabff3c21820eb2c4565bfa570a2d1
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-14 22:22:35 +0530

    modified

--- src/ARRAYS/LongestEvenOddSubArray.java
+++ src/ARRAYS/LongestEvenOddSubArray.java
@@ -2,12 +2,25 @@ package ARRAYS;
 public class LongestEvenOddSubArray {
     static void LongestEvenOddSubarray (int [] arr){
         int n= arr.length;
-        int max=arr[0];
-        int sum=
-        for(int i=0; i<n; i++)
+        int count=0;
+        int sum=0;
+        if(arr.length>1){
+        for(int i=1; i<n; i++)
         {
-            if (arr[i]%2==0)
+            if (arr[i]%2!=arr[i-1]%2) {
+                sum++;
+            }
+            else {
+                if (sum>count)
+                {
+                    count=sum;
+                    sum=0;
+                }
+
+            }
         }
+        }
+        else System.out.println(arr[0]);
     }
     public static void main(String[] args) {
         int [] arr = {10,12,14,7,8};
