84fa5b45d58681c372e5f07f975a4fc69ed9a9ad
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-14 22:27:28 +0530

    modified

--- src/ARRAYS/LongestEvenOddSubArray.java
+++ src/ARRAYS/LongestEvenOddSubArray.java
@@ -21,7 +21,7 @@ public class LongestEvenOddSubArray {
         else System.out.println(arr[0]);
     }
     public static void main(String[] args) {
-        int [] arr = {1, 2, 3, 4, 5};
+        int [] arr = {1, 2, 3, 4, 5,7};
         LongestEvenOddSubarray(arr);
     }
 }
