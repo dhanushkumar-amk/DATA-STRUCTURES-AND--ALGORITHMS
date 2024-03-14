728b459a04f747f4beb63eb7f3f9bab15f5454d3
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-14 18:56:35 +0530

    Automatic commit

--- src/ARRAYS/LongestEvenOddSubArray.java
+++ src/ARRAYS/LongestEvenOddSubArray.java
@@ -11,6 +11,6 @@ public class LongestEvenOddSubArray {
         LongestEvenOddSubarray(arr);
         System.out.println("Arunkumar");
         System.out.println();
-        
+        System.out.println("arunkumar");
     }
 }
