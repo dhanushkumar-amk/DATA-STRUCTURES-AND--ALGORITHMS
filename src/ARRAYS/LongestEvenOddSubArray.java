513a663af0a72c12a7d33ab9a92e40f304f5f7e5
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-14 22:35:17 +0530

    modified

--- src/ARRAYS/LongestEvenOddSubArray.java
+++ src/ARRAYS/LongestEvenOddSubArray.java
@@ -7,12 +7,9 @@ public class LongestEvenOddSubArray {
         if(arr.length>1){
         for(int i=1; i<n; i++)
         {
-            if (sum>count) {
-                count = sum;
-                sum=1;
-            }
             if (arr[i]%2==0 && arr[i-1]%2!=0 ||arr[i]%2!=0 &&arr[i-1]%2==0) {
                 sum++;
+                count=Integer.max(sum,count);
             }
             else {
                 sum = 1;
