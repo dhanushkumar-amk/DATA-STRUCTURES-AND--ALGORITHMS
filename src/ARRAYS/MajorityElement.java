62537fb73729878a47be82dc3cd2ee2f7ba40b72
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-16 17:01:32 +0530

    modified

--- src/ARRAYS/MajorityElement.java
+++ src/ARRAYS/MajorityElement.java
@@ -1,32 +1,35 @@
 package ARRAYS;
 public class MajorityElement {
     static int MajorityElement (int [] arr ,int n) {
-        int i = 0;
-        while (i < n) {
-            if (arr[i] <= 0 || arr[i] > n) {
+        int i=0;
+        int N=n;
+        while(i<N)
+        {
+            if(arr[i]<=0||arr[i]>N)
+            {
                 i++;
                 continue;
             }
-            int ind = arr[i] - 1;
-            if (arr[ind] > 0) {
-                arr[i] = arr[ind];
-                arr[i] = -1;
-            } else {
+            int  ind=arr[i]-1;
+            if(arr[ind]>0)
+            {
+                arr[i]=arr[ind];
+                arr[ind]=-1;
+            }
+            else {
                 arr[ind]--;
-                arr[i] = 0;
+                arr[i]=0;
                 i++;
             }
 
+        }
+        for(int j=0;j<N; j++){
+            if(arr[j]<0)
+                arr[j]=-arr[j];
+            else arr[j]=0;
 
         }
-        for (int j = 0; j < n; j++) {
-            if (arr[j] < 0)
-                arr[j] = -arr[j];
-            else arr[j] = 0;
-        }
-            for (int temp : arr)
-                System.out.print(temp + " ");
-            return 10;
+        return 1;
         }
 
     public static void main(String[] args) {
