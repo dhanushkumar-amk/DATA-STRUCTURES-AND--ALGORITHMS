d00cfa7766b413484b7413e4ba0c07edd5b70f24
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-16 12:03:09 +0530

    modified

--- src/ARRAYS/MaximumCircularSumSubArray.java
+++ src/ARRAYS/MaximumCircularSumSubArray.java
@@ -1,31 +1,30 @@
 package ARRAYS;
  class MaximumCircularSumSubArray {
-    static int MaxiMumCircularSumSubArrays(int[] arr,int n) {
-        int max=Integer.MIN_VALUE;
-        int min=Integer.MAX_VALUE;
-        int Totalsum=0 ;
-        int currMax=0;
-        int currMin=0;
-        for(int x:arr)
-        {
-            Totalsum+=x;
+     static int MaxiMumCircularSumSubArrays(int[] arr, int n) {
+         int max = Integer.MIN_VALUE;
+         int min = Integer.MAX_VALUE;
+         int Totalsum = 0;
+         int currMax = 0;
+         int currMin = 0;
+         for (int x : arr) {
+             Totalsum += x;
 
-            currMax+=x;
-            if(currMax>max) max=currMax;
-            if(currMax<0)currMax=0;
+             currMax += x;
+             if (currMax > max) max = currMax;
+             if (currMax < 0) currMax = 0;
 
-            currMin+=x;
-            if(currMin<0) min=currMin;
-            if(currMin>0) currMin=0;
-        }
-        if(max<0) return max;
-        return Math.max(max,Totalsum-min);
-    }
-    }
-    public static void main(String[] args) {
-        int[] arr = {10,-3,-4,7,6,5,-4,-1};
-        int n= arr.length;
-      int result=  MaxiMumCircularSumSubArrays(arr,n);
-        System.out.println(result);
-    }
-}
+             currMin += x;
+             if (currMin < 0) min = currMin;
+             if (currMin > 0) currMin = 0;
+         }
+         if (max < 0) return max;
+         return Math.max(max, Totalsum - min);
+     }
+
+     public static void main(String[] args) {
+         int[] arr = {10, -3, -4, 7, 6, 5, -4, -1};
+         int n = arr.length;
+         int result = MaxiMumCircularSumSubArrays(arr, n);
+         System.out.println(result);
+     }
+ }
