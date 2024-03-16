1232fe130b5e9f123e06681aa86ce2e80423472d
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-16 16:21:13 +0530

    modified

--- src/ARRAYS/MajorityElement.java
+++ src/ARRAYS/MajorityElement.java
@@ -4,10 +4,10 @@ public class MajorityElement {
 
         for(int i=0; i<n; i++) {
             if (arr[i] > 0 && (arr[i] < n)) {
-                if (arr[arr[i-1]] < 0) {
-                    arr[arr[i-1]] -= 1;
+                if (arr[arr[i]] < 0) {
+                    arr[arr[i]] -= 1;
                 }
-                else arr[arr[i-1]]=-1;
+                else arr[arr[i]]=-1;
             }
         }
         for (int temp:arr)
