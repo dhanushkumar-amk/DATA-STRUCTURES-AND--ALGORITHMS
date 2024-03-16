a04345bf078ff2b8fe7ee5cfb95be84295560200
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-16 16:09:15 +0530

    modified

--- src/ARRAYS/MajorityElement.java
+++ src/ARRAYS/MajorityElement.java
@@ -1,7 +1,14 @@
 package ARRAYS;
 public class MajorityElement {
     static int MajorityElement (int [] arr ,int n) {
-        
+
+        for(int i=0; i<n; i++)
+        {
+          if(arr[i]>0)
+              arr[arr[i]-1]=-1;
+        }
+        for (int temp:arr)
+            System.out.print(temp+" ");
     }
     public static void main(String[] args) {
         int []arr = {5,4,13,5,5,4,5};
