60eff741ad06f3aa0e23f888eb47b1ea37b6cc01
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-16 16:02:40 +0530

    modified

--- src/ARRAYS/MajorityElement.java
+++ src/ARRAYS/MajorityElement.java
@@ -1,2 +1,11 @@
-package ARRAYS;public class MajorityElement {
+package ARRAYS;
+public class MajorityElement {
+    static int MajorityElement (int [] arr ,int n) {
+        
+    }
+    public static void main(String[] args) {
+        int []arr = {5,4,13,5,5,4,5};
+       int res= MajorityElement(arr, arr.length);
+        System.out.println(res);
+    }
 }
