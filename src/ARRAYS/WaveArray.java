07fde74b35c57936a1f151f215abe13d61028148
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-16 15:59:48 +0530

    modified

--- src/ARRAYS/WaveArray.java
+++ src/ARRAYS/WaveArray.java
@@ -3,7 +3,7 @@ public class WaveArray {
     public static void main(String[] args) {
         int []arr ={1,2,3,4,5};
         int n= arr.length;
-        for (int i=0;  i<n; i++)
+        for (int i=1;  i<n; i++)
         {
             int temp=arr[i];
             arr[i]=arr[i-1];
