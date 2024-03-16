7c8ae541a986b8921524fa63f18f400974f40d37
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-16 12:06:25 +0530

    modified

--- src/ARRAYS/MaximumCircularSumSubArray.java
+++ src/ARRAYS/MaximumCircularSumSubArray.java
@@ -24,7 +24,7 @@ package ARRAYS;
      }
 
      public static void main(String[] args) {
-         int[] arr = {10,-3,-4,7,6,5,-4,-1};
+         int[] arr = {8,-8,9,-9,10,-11,12};
          int n = arr.length;
          int result = MaxiMumCircularSumSubArrays(arr, n);
          System.out.println(result);
