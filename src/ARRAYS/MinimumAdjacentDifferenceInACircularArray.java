df573241e9a312fd886b14fcb9fd7cb4afc9e5d2
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-16 14:57:26 +0530

    modified

--- src/ARRAYS/MinimumAdjacentDifferenceInACircularArray.java
+++ src/ARRAYS/MinimumAdjacentDifferenceInACircularArray.java
@@ -1,6 +1,18 @@
 package ARRAYS;
 public class MinimumAdjacentDifferenceInACircularArray {
     public static void main(String[] args) {
-
+        int [] arr ={}
+        int curr=0;
+        int min=Integer.MAX_VALUE;
+        int temp=0;
+        for(int i=1;i<n; i++)
+        {
+            temp=Math.abs(arr[i]-arr[i-1]);
+            if(temp<min)
+                min=temp;
+        }
+        if(arr[n-1]-arr[0]<min)
+            min=arr[n-1]-arr[0];
+        return min;
     }
 }
