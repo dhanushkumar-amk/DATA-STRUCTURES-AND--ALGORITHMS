ab3c0cb7e98a5eb6eb68a158f5bcb3888dd74b99
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-17 21:02:03 +0530

    modified

--- src/ARRAYS/WhoIsMajor.java
+++ src/ARRAYS/WhoIsMajor.java
@@ -18,12 +18,13 @@ public class WhoIsMajor {
         result=x1>y1?x:y;
         if(x1==y1)
             return Math.min(x,y);
+
         return result;
     }
     public static void main(String[] args) {
         int [] arr ={1,2,3,4,5,6,7,8};
-        int x=4;
-        int y=5;
+        int x=1;
+        int y=7;
       int result=  Major(arr,x,y);
         System.out.println(result);
     }
