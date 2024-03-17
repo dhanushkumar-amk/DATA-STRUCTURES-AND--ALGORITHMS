2154bb9e8bd5967f4ca53565b9acbe578d52b165
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-17 16:35:56 +0530

    modified

--- src/ARRAYS/MaximumOccuredInteger.java
+++ src/ARRAYS/MaximumOccuredInteger.java
@@ -1,8 +1,13 @@
 package ARRAYS;
 
 class  MaximumOccuredInteger{
-    static int MaximumOccuredInt() {
-        int 
+    static int MaximumOccuredInt(int [] l,int [] r, int n) {
+        int leftMin=l[0];
+        int rightMin=r[0];
+        for (int i=1; i<n; i++)
+        {
+            if (leftMin>)
+        }
         return -1;
     }
     public static void main(String[] args) {
