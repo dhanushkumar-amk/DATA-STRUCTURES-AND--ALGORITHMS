90619d34567f246cbbb1d3279eebcd0db8d627eb
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-17 13:50:53 +0530

    modified

--- src/ARRAYS/MajorityElement.java
+++ src/ARRAYS/MajorityElement.java
@@ -22,7 +22,7 @@ public class MajorityElement {
         return -1;
     }
         public static void main (String[]args){
-            int[] arr = {1,1,3};
+            int[] arr = {3,1,3,3,2};
             int res = MajorityElement(arr, arr.length);
             System.out.println(res);
         }
