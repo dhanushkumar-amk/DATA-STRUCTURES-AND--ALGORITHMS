03f45cb5f60abc3e1d557c7c8895023069fb9445
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-17 13:35:30 +0530

    modified

--- src/ARRAYS/MajorityElement.java
+++ src/ARRAYS/MajorityElement.java
@@ -1,45 +1,21 @@
 package ARRAYS;
 public class MajorityElement {
     static int MajorityElement (int [] arr ,int n) {
-        int i=0;
-        int N=n;
-        int last=arr[n-1];
-        while(i<N)
+
+        int currMajor=arr[0];
+        int count=1;
+        for(int i=1;i<n; i++ )
         {
-            if(arr[i]<=0||arr[i]>N)
-            {
-                i++;
+            if(count==0){
+                currMajor=arr[i];
+                count=1;
                 continue;
             }
-            int  ind=arr[i]-1;
-            if(arr[ind]>0)
-            {
-                arr[i]=arr[ind];
-                arr[ind]=-1;
+            if(currMajor==a[i])
+                count++;
+            else{
+                count--;
             }
-            else {
-                arr[ind]--;
-                arr[i]=0;
-                i++;
-            }
-
-        }
-        for(int j=0;j<N; j++){
-            if(arr[j]<0)
-                arr[j]=-arr[j];
-            else arr[j]=0;
-        }
-        for (int x:arr)
-            System.out.print(x+" ");
-        int j;
-        for (j=0; j<n; j++)
-        {
-            if(arr[j]>n/2)
-                return j+1;
-        }
-        if (j==n-1)
-            return last;
-        return -1;
     }
 
     public static void main(String[] args) {
