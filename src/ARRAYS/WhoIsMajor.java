c2ec670c8735ae26b118596039d9e381c94ec38c
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-17 21:00:51 +0530

    modified

--- src/ARRAYS/WhoIsMajor.java
+++ src/ARRAYS/WhoIsMajor.java
@@ -1,2 +1,27 @@
-package ARRAYS;public class WhoIsMajor {
+package ARRAYS;
+
+public class WhoIsMajor {
+    static int Major(int[] arr,int n) {
+        int x1=0;
+        int y1=0;
+        int result=0;
+
+        for(int i=0; i<n; i++)
+        {
+
+            if(x==arr[i])
+                x1++;
+
+            if(y==arr[i])
+                y1++;
+        }
+        result=x1>y1?x:y
+        if(x1==y1)
+            return Math.min(x,y);
+        return result;
+    }
+    public static void main(String[] args) {
+        int [] arr ={}
+        Major()
+    }
 }
