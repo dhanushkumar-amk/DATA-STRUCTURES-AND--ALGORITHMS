fa8c197355334899a09856fb487929188f29521a
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-17 14:33:38 +0530

    modified

--- src/ARRAYS/MajorityElement.java
+++ src/ARRAYS/MajorityElement.java
@@ -1,8 +1,8 @@
 package ARRAYS;
 public class MajorityElement {
     static int MajorityElement(int[] arr, int n) {
-        int majority=arr[0];
-        int count=1;
+        int majority=0;
+        int count=0;
         for (int i=1; i<n; i++)
         {
             if (count==0)
