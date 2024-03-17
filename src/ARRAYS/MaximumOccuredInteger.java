f696e6e926db5e68c8975340c3c709f0785744dd
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-17 17:00:28 +0530

    modified

--- src/ARRAYS/MaximumOccuredInteger.java
+++ src/ARRAYS/MaximumOccuredInteger.java
@@ -16,7 +16,7 @@ class  MaximumOccuredInteger{
         return -1;
     }
     public static void main(String[] args) {
-        int [] arr1={1,5,9,13,21};
+        int [] arr1={1,4,3,1};
         int [] arr2={15,8,12,20,30};
         MaximumOccuredInt(arr1,arr2,arr1.length);
     }
