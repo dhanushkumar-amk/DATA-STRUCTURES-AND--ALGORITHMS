03b3347930f4dc994b94db86aadf5f416b7d594c
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-17 14:03:52 +0530

    modified

--- src/ARRAYS/MajorityElement.java
+++ src/ARRAYS/MajorityElement.java
@@ -1,32 +1,29 @@
 package ARRAYS;
 public class MajorityElement {
     static int MajorityElement(int[] arr, int n) {
-        int majority = arr[0];
-        int count = 1;
-        for (int i = 1; i < n; i++) {
-            if (arr[i] == majority) {
-                count++;
-            } else {
-                count--;
-                if (count == 0) {
-                    majority = arr[i];
-                    count = 1;
-                }
-            }
-        }
+        int majority=arr[0];
+        int count=1;
+        for (int i=0; i<n-1; i++) {
 
-        count = 0; // Reset count to count occurrences of majority element
-        for (int i = 0; i < n; i++) {
-            if (arr[i] == majority) {
-                count++;
+            if (arr[i] == arr[i + 1]) {
+                 count++;
+
             }
+            else {
+               if(count>0) {
+                   count--;
+               }
+               else {
+                   majority=arr[i+1];
+                   count=1;
+               }
+            }
+
         }
-        // Check if the majority element actually appears more than n/2 times
         if (count > n / 2) {
             return majority;
-        } else {
-            return -1;
         }
+        return -1;
     }
         public static void main (String[]args){
             int[] arr = {3,1,3,3,2};
