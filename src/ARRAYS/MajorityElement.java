843708d8232f6d9b4ba928b51385c37a89178e03
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-17 13:47:31 +0530

    modified

--- src/ARRAYS/MajorityElement.java
+++ src/ARRAYS/MajorityElement.java
@@ -7,9 +7,17 @@ public class MajorityElement {
         {
             if(arr[i]==arr[i+1])
                 count++;
-            count--;
-            
+            else {
+               if(count>0) {
+                   count--;
+               }
+               else {
+                   majority=arr[i+1];
+                   count=1;
+               }
+            }
         }
+        System.out.println(majority);
     }
         public static void main (String[]args){
             int[] arr = {4,1,1,4,3,4,4};
