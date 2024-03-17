2a41d1ec32d405b5e1f2fde241d2dc7732dfaf7f
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-17 18:12:01 +0530

    modified

--- src/ARRAYS/MaximumOccuredInteger.java
+++ src/ARRAYS/MaximumOccuredInteger.java
@@ -7,7 +7,7 @@ class  MaximumOccuredInteger{
         HashMap<Integer,Integer> hashMap = new HashMap();
         for (int i=0; i<n; i++)
         {
-            for (int j=l[i]; j<r[i];j++){
+            for (int j=l[i]; j<=r[i];j++){
                 if (!hashMap.containsKey(j))
                 {
                     hashMap.put(j,1);
