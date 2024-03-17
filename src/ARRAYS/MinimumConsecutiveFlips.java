aad9d0b4bcd890b961236b502ed223cfbdc9e35b
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-17 21:22:03 +0530

    modified

--- src/ARRAYS/MinimumConsecutiveFlips.java
+++ src/ARRAYS/MinimumConsecutiveFlips.java
@@ -1,7 +1,19 @@
 package ARRAYS;
 public class MinimumConsecutiveFlips {
+    static int MinimumConsecutiveFlip(int [] arr,int n) {
+        int Zeros=0;
+        int Ones=0;
+        for (int i=0; i<n; i++)
+        {
+            if(arr[i]==0)
+                Zeros++;
+            else  Ones++;
+        }
+        return -1;
+    }
     public static void main(String[] args) {
-
+        int [] arr ={0,1,1,1,0};
+        int n= arr.length;
+        MinimumConsecutiveFlip(arr,n);
     }
 }
-
