031ed4ebf1015914fcfee2c5e079eb47d07a8c99
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-17 16:46:57 +0530

    modified

--- src/ARRAYS/MaximumOccuredInteger.java
+++ src/ARRAYS/MaximumOccuredInteger.java
@@ -4,11 +4,13 @@ class  MaximumOccuredInteger{
     static int MaximumOccuredInt(int [] l,int [] r, int n) {
         int leftMax=l[0];
         int rightMin=r[0];
+        //&&rightMin<=l[i]
+        //&&leftMax>=r[i]
         for (int i=1; i<n; i++)
         {
-            if (leftMax<l[i]&&rightMin<=l[i])
+            if (leftMax<l[i])
                 leftMax=l[i];
-            if(rightMin>r[i]&&leftMax>=r[i])
+            if(rightMin>r[i])
                 rightMin=r[i];
         }
         System.out.println(leftMax+" "+rightMin);
