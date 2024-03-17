045e6c3d4ca0253de8dd498b8e8dc391203fb8e4
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-17 19:19:13 +0530

    modified

--- src/ARRAYS/MaximumOccuredInteger.java
+++ src/ARRAYS/MaximumOccuredInteger.java
@@ -26,8 +26,8 @@ class  MaximumOccuredInteger{
     static int MaxiOccured(int[] l,int []r,int n,int max) {
         int [] freq = new int[max];
         for (int i=1 ;i<n; i++){
-            freq[l[i]]+=1;
-            freq[r[i]]-=1;
+            freq[l[i-1]]+=1;
+            freq[r[i-1]]-=1;
         }
         int result=0;
         for (int i=1; i<n; i++)
