cb5972c51303f4fbbdf24b01e212d045c7919ccd
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-18 18:12:00 +0530

    modified

--- src/ARRAYS/MinimumConsecutiveFlips.java
+++ src/ARRAYS/MinimumConsecutiveFlips.java
@@ -29,7 +29,7 @@ public class MinimumConsecutiveFlips {
                 }
             }
         }
-        System.out.println();
+        System.out.println(start);
         return 0;
     }
     public static void main(String[] args) {
