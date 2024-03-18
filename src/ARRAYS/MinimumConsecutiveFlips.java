65ccecca313731ed0209abf1fdbbd3523f57d354
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-18 16:36:56 +0530

    modified

--- src/ARRAYS/MinimumConsecutiveFlips.java
+++ src/ARRAYS/MinimumConsecutiveFlips.java
@@ -1,21 +1,36 @@
 package ARRAYS;
+
+import com.sun.security.auth.NTSidGroupPrincipal;
+
 public class MinimumConsecutiveFlips {
-    static int MinimumConsecutiveFlip(int [] arr,int n) {
-        int ones=1;
-        int zeros=1;
-        for (int i=1; i<n; i++)
-        {
-            if(arr[i]!=arr[i-1])
-                if(arr[i-1]==1)
+    static int MinimumConsecutiveFlip(int[] arr, int n) {
+        int ones = 1;
+        int zeros = 1;
+        boolean consBool = false;
+        for (int i = 1; i < n; i++) {
+            if (arr[i] != arr[i - 1])
+                if (arr[i - 1] == 1)
                     ones++;
-            else zeros++;
+                else zeros++;
         }
-        if(ones==zeros)
-            return zeros;
-        return  Math.min(ones,zeros);
+        if (ones == zeros)
+            consBool = false;
+        if (ones > zeros)
+            consBool = true;
+        else consBool = false;
+        int ind = 0;
+        for (int i = 1; i < n; i++) {
+            if (consBool == true) {
+                if (arr[i - 1] != arr[i]) {
+                    System.out.println();
+                }
+            }
+        }
+    return -1;
+
     }
     public static void main(String[] args) {
-        int [] arr ={0,1,0,1};
+        int [] arr ={1,1,0,0,0,1,0,0,1,1,0,1};
         int n= arr.length;
       int result=  MinimumConsecutiveFlip(arr,n);
         System.out.println(result);
