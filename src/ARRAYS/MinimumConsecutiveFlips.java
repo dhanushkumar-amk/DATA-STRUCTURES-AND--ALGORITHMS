515c5e7be40aa8a0e4281af3af3edd3530bef139
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-18 16:39:48 +0530

    modified

--- src/ARRAYS/MinimumConsecutiveFlips.java
+++ src/ARRAYS/MinimumConsecutiveFlips.java
@@ -21,8 +21,16 @@ public class MinimumConsecutiveFlips {
         int ind = 0;
         for (int i = 1; i < n; i++) {
             if (consBool == true) {
+
                 if (arr[i - 1] != arr[i]) {
-                    System.out.println();
+                    System.out.println("From "+ind+" to "+(i-1));
+                    ind=i;
+                }
+                else {
+                    if (arr[i - 1] != arr[i]) {
+                        System.out.println("From "+ind+" to "+(i-1));
+                        ind=i;
+                    }
                 }
             }
         }
