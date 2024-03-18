d2de342183552cab597362bee90e2153ccecbbe9
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-18 20:19:10 +0530

    modified

--- src/ARRAYS/MinimumConsecutiveFlips.java
+++ src/ARRAYS/MinimumConsecutiveFlips.java
@@ -6,14 +6,20 @@ public class MinimumConsecutiveFlips {
     static int MinimumConsecutiveFlip(int[] arr, int n) {
         int s=0;
         int e=0;
-        for(int i=1; i<n-1;i++)
-                if(arr[i]!=arr[i-1])
-                    if (arr[i] != arr[0])
-                        System.out.print("From "+i+" to ");
-                       else if(i==i-1)
-                               System.out.println(i+" ");
-                           else System.out.println((i-1));
-        return 0;
+        for (int i = 1; i < n; i++) {
+            if (arr[i] != arr[i - 1]) {
+                if (arr[i] != arr[0]) {
+                    System.out.print("From " + i + " to ");
+                } else {
+                    if (i == n - 1) {
+                        System.out.println(i);
+                    } else {
+                        System.out.print((i - 1) + " ");
+                    }
+                }
+            }
+        }
+        return -1;
     }
     public static void main(String[] args) {
         int [] arr ={1,1,1,0,1};
