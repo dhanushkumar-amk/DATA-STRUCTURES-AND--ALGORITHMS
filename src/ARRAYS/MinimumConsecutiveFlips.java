a7474923d17d32d1faba8b039b5a0a0a90b76825
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-18 17:07:01 +0530

    modified

--- src/ARRAYS/MinimumConsecutiveFlips.java
+++ src/ARRAYS/MinimumConsecutiveFlips.java
@@ -15,7 +15,7 @@ public class MinimumConsecutiveFlips {
                 else zeros++;
             }
         }
-        if(n-1==1)
+        if(arr[n-1]==1)
             ones++ ;
         else zeros++;
 
