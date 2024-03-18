55ecfd5562af25eadccd7fa5a5caccb2ec12d532
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-18 18:29:42 +0530

    modified

--- src/ARRAYS/MinimumConsecutiveFlips.java
+++ src/ARRAYS/MinimumConsecutiveFlips.java
@@ -4,44 +4,12 @@ import com.sun.security.auth.NTSidGroupPrincipal;
 
 public class MinimumConsecutiveFlips {
     static int MinimumConsecutiveFlip(int[] arr, int n) {
-        int ones = 0;
-        int zeros = 0;
-        int previous=0;
-        String start="";
-        String end="";
-        for (int i = 1; i < n; i++) {
-            previous = arr[i - 1];
-            if (arr[i] != previous) {
-                if (previous == 1) {
-                    ones++;
-
-                } else {
-                    zeros++;
-                }
-
-            }
-            if (i == n - 1) {
-                if (arr[n - 1] == 1) {
-                    ones++;
-
-                } else {
-                    zeros++;
-                }
-            }
-        }
-        boolean bin=ones>zeros?false:true;
-
         for(int i=1; i<n-1;i++)
-        {
-
-                if(arr[i]!=arr[i-1]) {
+                if(arr[i]!=arr[i-1])
                     if (arr[i] != arr[0])
                         System.out.print(i + " ");
-                    else
-                        System.out.print((i-1)+" ");
-                }
-
-        }
+                       else
+                           System.out.print((i-1)+" ");
         return 0;
     }
     public static void main(String[] args) {
