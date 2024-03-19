9c2e4e77324efa9ffeafa8a092a72a56c1b015e5
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-19 21:41:32 +0530

    modified

--- src/ARRAYS/PrintPatternUsingRecursion.java
+++ src/ARRAYS/PrintPatternUsingRecursion.java
@@ -1,2 +1,22 @@
-package ARRAYS;public class PrintPatternUsingRecursion {
+package ARRAYS;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class PrintPatternUsingRecursion {
+    static void pattern(int N, ArrayList<Integer> arrayList) {
+        if(N<0)
+            return;
+        arrayList.add(N);
+        pattern(N-5,arrayList);
+       arrayList.add(N);
+
+    }
+    public static void main(String[] args) {
+         int n=16;
+         ArrayList<Integer> integerArrayList = new ArrayList<>();
+         pattern(n,integerArrayList);
+         for (int x:integerArrayList)
+             System.out.print(x+" ");
+    }
 }
