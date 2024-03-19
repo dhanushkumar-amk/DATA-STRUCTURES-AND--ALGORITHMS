82a355f85a1b20373b3e55c24a84ca53a76754a9
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-19 20:59:37 +0530

    modified

--- src/ARRAYS/MaximumSumSubArrayOfSizeK.java
+++ src/ARRAYS/MaximumSumSubArrayOfSizeK.java
@@ -33,8 +33,8 @@ public class MaximumSumSubArrayOfSizeK {
         mSum=Math.max(cSum,mSum);
         for (int j=K; j<N; j++)
         {
-            cSum-=Arr[j-K];
-            cSum+=Arr[j];
+            cSum-=Arr[j-K]+Arr[j];
+
             j++;
             mSum=Math.max(cSum,mSum);
         }
