e64bbc3e6eb756caaf805887b8dda02e0d7ae5e3
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-19 19:19:32 +0530

    modified

--- src/ARRAYS/MaximumSumSubArrayOfSizeK.java
+++ src/ARRAYS/MaximumSumSubArrayOfSizeK.java
@@ -4,9 +4,9 @@ public class MaximumSumSubArrayOfSizeK {
     public static void main(String[] args) {
         int curSum=0;
         int maxSum=Integer.MIN_VALUE;
-        int arr[] = {10, -7, 8, -6, 13, 5, 12, -9, 4, -3, 15, -2, 7};
+        int arr[] = {2, 3, 5, -7, 8, -6, 13, 5, 4, 12, -9, 4, -3, 15, -2, 7, 1, 6, 9, -4, 11, 8, 3, -5};
         int n=arr.length;
-        int k=1;
+        int k=2;
         int count=0;
         for (int i=0; i<=n-k;)
         {
