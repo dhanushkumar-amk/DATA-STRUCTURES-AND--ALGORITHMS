cbe1460b51239ffab4bcd80706fcc7d0f8f75b7f
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-20 18:28:27 +0530

    modified

--- src/ARRAYS/LongestSubArrayOfEvensAndOdds.java
+++ src/ARRAYS/LongestSubArrayOfEvensAndOdds.java
@@ -1,7 +1,15 @@
 package ARRAYS;
 public class LongestSubArrayOfEvensAndOdds {
     static int LongestSubArrayEvenAndOdd(int [] arr,int n) {
-        
+        int sum=0;
+        int maxLen=1;
+        for(int i=1; i<n; i++)
+        {
+            if(arr[i]%2!=arr[i-1]%2)
+                sum++;
+        }
+        System.out.println(sum);
+        return -1;
     }
     public static void main(String[] args) {
         int [] arr ={10,12,14,7,8};
