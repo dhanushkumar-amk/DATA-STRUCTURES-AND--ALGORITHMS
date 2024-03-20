f42cac95020895e4a0ea3eeb7f8f6dd1afd88380
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-20 17:29:08 +0530

    modified

--- src/ARRAYS/SubArrayWithGivenSum.java
+++ src/ARRAYS/SubArrayWithGivenSum.java
@@ -7,33 +7,28 @@ public class SubArrayWithGivenSum {
     static ArrayList<Integer> subarraySum(int[] arr, int n, int s)
     {
         ArrayList<Integer> arrayList = new ArrayList();
+
         int i=0;
-        int cSum=arr[0];
         int j=0;
-        while (i<n) {
-            if (cSum == s) {
-                arrayList.add(i+1);
-                arrayList.add(j+1);
-                return arrayList;
+        int sum=arr[0];
+        while (i<n)
+        {
+            if(sum==s) {
+                arrayList.add(i + 1);
+                arrayList.add(j + 1);
             }
-            if(cSum<s)
+            if(sum<s)
             {
                 j++;
-                cSum+=arr[j];
-                continue;
+                sum+=arr[j];
             }
-            else {
-                arrayList.clear();
+            if(sum>s)
+            {
+                sum-=arr[i];
                 i++;
-                j=i;
             }
         }
-        if(arrayList.size()==0)
-        {
-            arrayList.add(-1);
-            return arrayList;
-        }
-        else return arrayList;
+        return arrayList
     }
     public static void main(String[] args) {
         int [] arr ={1,2,3,4,5,7};
