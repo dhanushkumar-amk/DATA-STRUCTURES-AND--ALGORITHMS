fd1e65beda62a2a169352e36d32c2ce9fe4e2ee7
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-20 18:28:01 +0530

    modified

--- src/ARRAYS/LongestSubArrayOfEvensAndOdds.java
+++ src/ARRAYS/LongestSubArrayOfEvensAndOdds.java
@@ -1,2 +1,10 @@
-package ARRAYS;public class LongestSubArrayOfEvensAndOdds {
+package ARRAYS;
+public class LongestSubArrayOfEvensAndOdds {
+    static int LongestSubArrayEvenAndOdd(int [] arr,int n) {
+
+    }
+    public static void main(String[] args) {
+        int [] arr ={10,12,14,7,8};
+        LongestSubArrayEvenAndOdd()
+    }
 }
