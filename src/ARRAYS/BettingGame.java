f51d568593dc20bfb3f7f8c0972b15d8dff39bc4
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-20 22:22:21 +0530

    modified

--- src/ARRAYS/BettingGame.java
+++ src/ARRAYS/BettingGame.java
@@ -2,6 +2,23 @@ package ARRAYS;
 
 public class BettingGame {
     public static void main(String[] args) {
-        
+        int bet =1;
+        int res=0;
+        int balance=4;
+        for(int i=0; i<result.length(); ++i)
+        {
+            if(balance<=0)
+                return -1;
+            if(result.charAt(i)=='W')
+            {
+                balance+=bet;
+            }
+            if(result.charAt(i)=='L')
+            {
+                balance-=bet;
+                bet*=2;
+            }
+        }
+        return balance;
     }
 }
