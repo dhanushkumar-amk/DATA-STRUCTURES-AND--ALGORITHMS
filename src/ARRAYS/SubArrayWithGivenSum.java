aa2a11e627b9a482a48e22ebb04bd2dcb840bf5d
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-20 16:57:18 +0530

    modified

--- src/ARRAYS/SubArrayWithGivenSum.java
+++ src/ARRAYS/SubArrayWithGivenSum.java
@@ -24,9 +24,9 @@ public class SubArrayWithGivenSum {
         return -1;
     }
     public static void main(String[] args) {
-        int [] arr ={1,2,3,4,5,6,7,8,9,10};
+        int [] arr ={1,2,3};
         int n= arr.length;;
-        int s=15;
+        int s=3;
         SubArrayWithGivenSum(arr,n,s);
 
     }
