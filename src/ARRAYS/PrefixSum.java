3e4a631131480a94744ee95e52176ff3eb39a143
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-21 11:07:23 +0530

    modified

--- src/ARRAYS/PrefixSum.java
+++ src/ARRAYS/PrefixSum.java
@@ -1,33 +1,14 @@
 package ARRAYS;
 public class PrefixSum {
-    static int []prefixSumAdding() {
-        int [] arr ={2, 8, 3, 9, 6, 5, 4};
-        for (int i=1; i< arr.length; i++)
+    static  void prefixSumAdding(int[]arr) {
+        for (int i=0; i< arr.length;++i)
         {
-            arr[i]=arr[i-1]+arr[i];
+            
         }
-        for (int x:arr)
-            System.out.print(x+" ");
-        System.out.println();
-        System.out.println(arr[2]-arr[0]);
-        return arr;
     }
-    static  void prefixSum (int [] arr) {
-        System.out.println(sum(arr,0,2));
-        System.out.println(sum(arr,1,3));
-        System.out.println(sum(arr,2,6));
-    }
-    static int sum(int [] arr,int i,int j) {
-        int sum=0;
-        for (;i<=j;i++)
-        {
-         sum+=arr[i];
-        }
-        return sum;
-    }
+
     public static void main(String[] args) {
      int arr[]  ={2,8,3,9,6,5,4};
-//     prefixSum(arr);
-     prefixSumAdding();
+     prefixSumAdding(arr);
     }
 }
