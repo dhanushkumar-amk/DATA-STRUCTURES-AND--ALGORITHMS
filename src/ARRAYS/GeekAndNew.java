2080264e0329e6f06e5e5516b9caabde6b90e5cc
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-21 14:35:00 +0530

    modified

--- src/ARRAYS/GeekAndNew.java
+++ src/ARRAYS/GeekAndNew.java
@@ -3,7 +3,7 @@ package ARRAYS;
 public class GeekAndNew {
     public static void main(String[] args) {
         int T= 2;
-        String str="Geek";
+        String str="Gee1k";
         int n=str.length();
         boolean flag=true;
         for(int i=0; i<n; i++) {
