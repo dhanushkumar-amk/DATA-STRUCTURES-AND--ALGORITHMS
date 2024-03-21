536a6384f8475ff634fbb20125598a7c4ede7ee8
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-21 15:40:24 +0530

    modified

--- src/MATHEMATICAL/NearstPerfectSquare.java
+++ src/MATHEMATICAL/NearstPerfectSquare.java
@@ -1,13 +1,14 @@
 package MATHEMATICAL;
 public class NearstPerfectSquare {
     public static void main(String[] args) {
-        long n=5;
-        long first=0;
-        long second=0;
-        if(n-1%2==0)
-            first=n-1;
-        if(n+1%2==0)
-            second=n+1;
+        int n=5;
+        long first = 0;
+        long second = 0;
+        if ((n - 1) % 2 == 0)
+            first = n - 1;
+        if ((n + 1) % 2 == 0)
+            second = n + 1;
+        return Math.max(first, second);
         System.out.println(Math.max(first,second));
     }
 }
