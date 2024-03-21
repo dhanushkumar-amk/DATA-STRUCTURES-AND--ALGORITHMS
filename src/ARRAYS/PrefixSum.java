577d5ccdde0e98cb21d70b5296206cfe316391e3
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-21 10:13:03 +0530

    modified

--- src/ARRAYS/PrefixSum.java
+++ src/ARRAYS/PrefixSum.java
@@ -3,9 +3,12 @@ public class PrefixSum {
     public static void main(String[] args) {
         int [] arr ={2,8,3,9,6,5,4};
         int [] prefixSum= new int[arr.length];
+        prefixSum[0]=arr[0];
+        int prefix=arr[0];
         for (int i=1; i< arr.length; ++i)
         {
-            prefixSum+=arr[i
+            prefixSum[i-1]+=prefix+arr[i];
+            prefix=arr[i];
         }
     }
 }
