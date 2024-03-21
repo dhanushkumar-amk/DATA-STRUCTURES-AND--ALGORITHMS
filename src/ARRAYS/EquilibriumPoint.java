39f1aed76e3a4d15b09c9c95705ab57f5816e15e
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-21 12:31:36 +0530

    modified

--- src/ARRAYS/EquilibriumPoint.java
+++ src/ARRAYS/EquilibriumPoint.java
@@ -11,7 +11,11 @@ public class EquilibriumPoint {
        for (int i=0; i< arr.length; i++)
        {
            sum-=arr[i];
-
+           sum2+=arr[i];
+           if(sum==sum2) {
+               System.out.println(i);
+               break;
+           }
        }
     }
 }
