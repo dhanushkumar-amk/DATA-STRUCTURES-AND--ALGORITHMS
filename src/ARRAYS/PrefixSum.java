51a8aab27e9f6fe4acddbda55abc4ce0f4e13404
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-21 10:35:28 +0530

    modified

--- src/ARRAYS/PrefixSum.java
+++ src/ARRAYS/PrefixSum.java
@@ -8,7 +8,7 @@ public class PrefixSum {
         {
             prefixSum[i]=prefixSum[i]+prefixSum[i-1]+arr[i];
         }
-        System.out.println(prefixSum[1]-prefixSum[2]);
+        System.out.println(prefixSum[1]-prefixSum[3]);
         return  prefixSum;
     }
     static  void prefixSum (int [] arr) {
