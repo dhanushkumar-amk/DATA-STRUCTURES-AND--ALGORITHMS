2ccaf5051b450c55eb83ed87572f084f0287562a
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-21 15:38:55 +0530

    modified

--- src/MATHEMATICAL/NearstPerfectSquare.java
+++ src/MATHEMATICAL/NearstPerfectSquare.java
@@ -1,2 +1,13 @@
-package MATHEMATICAL;public class NearstPerfectSquare {
+package MATHEMATICAL;
+public class NearstPerfectSquare {
+    public static void main(String[] args) {
+        long n=5;
+        long first=0;
+        long second=0;
+        if(n-1%2==0)
+            first=n-1;
+        if(n+1%2==0)
+            second=n+1;
+        return Math.max(first,second);
+    }
 }
