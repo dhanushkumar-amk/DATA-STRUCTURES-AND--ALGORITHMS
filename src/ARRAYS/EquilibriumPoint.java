057d511635cbcf8b8240354cf92746ef9ccc7a85
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-21 12:20:54 +0530

    modified

--- src/ARRAYS/EquilibriumPoint.java
+++ src/ARRAYS/EquilibriumPoint.java
@@ -2,6 +2,24 @@ package ARRAYS;
 public class EquilibriumPoint {
     public static void main(String[] args) {
         int [] arr ={3,4,8,-9,20,6};
-        
+        int i=1;
+        int n= arr.length;
+        int l=0;
+        int r=n-1;
+        int leftSum=0;
+        int RightSum=0;
+        int Equ=0;
+        while (l<r)
+        {
+            leftSum+=arr[l]+arr[l+1];
+            RightSum+=arr[r]+arr[r-1];
+            l++;
+            r--;
+            if (leftSum==RightSum) {
+                Equ = (l / 2) + 1;
+                break;
+            }
+            
+        }
     }
 }
