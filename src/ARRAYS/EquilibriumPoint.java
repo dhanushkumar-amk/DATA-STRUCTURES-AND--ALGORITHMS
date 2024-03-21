8f5974abc5e281e08546a949af3ddf3444251ced
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-21 12:06:09 +0530

    modified

--- src/ARRAYS/EquilibriumPoint.java
+++ src/ARRAYS/EquilibriumPoint.java
@@ -1,2 +1,7 @@
-package ARRAYS;public class EquilibriumPoint {
+package ARRAYS;
+public class EquilibriumPoint {
+    public static void main(String[] args) {
+        int [] arr ={3,4,8,-9,20,6};
+        
+    }
 }
