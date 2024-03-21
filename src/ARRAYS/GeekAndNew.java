e1b1ef4afaacb7de74701f232fae337a5d82ece6
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-21 14:33:16 +0530

    modified

--- src/ARRAYS/GeekAndNew.java
+++ src/ARRAYS/GeekAndNew.java
@@ -1,2 +1,18 @@
-package ARRAYS;public class GeekAndNew {
+package ARRAYS;
+
+public class GeekAndNew {
+    public static void main(String[] args) {
+        int T= 2;
+        String str="Geek";
+        boolean flag=true;
+        for(int i=0; i<n; i++)
+        {
+            if(!int(str.charAt(i))>64)
+            {
+                flag=false;
+                break;
+            }
+        }
+        System.out.println(flag==true:"YES":"NO"?);
+    }
 }
