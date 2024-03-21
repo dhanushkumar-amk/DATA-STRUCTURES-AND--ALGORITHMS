ef5d906e4690e9e7b8615352c1669fea15aa3773
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-21 10:03:42 +0530

    modified

--- src/ARRAYS/PrefixSum.java
+++ src/ARRAYS/PrefixSum.java
@@ -1,6 +1,11 @@
 package ARRAYS;
 public class PrefixSum {
     public static void main(String[] args) {
-        
+        int [] arr ={2,8,3,9,6,5,4};
+        int 
+        for (int i=0; i< arr.length; ++i)
+        {
+
+        }
     }
 }
