1b9b67587fe8ed9fc8224d3ed9163a4e27fd271e
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-25 18:32:17 +0530

    modified

--- src/MATHEMATICAL/NearstPerfectSquare.java
+++ src/MATHEMATICAL/NearstPerfectSquare.java
@@ -2,12 +2,14 @@ package MATHEMATICAL;
 public class NearstPerfectSquare {
 
     public static int fun(int n) {
-        int x = 1, k;
-        if (n == 1)
+
+            int x = 1, k;
+            if (n == 1)
+                return x;
+            for (k = 1; k < n; ++k)
+                x = x + fun(k) * fun(n -k);
             return x;
-        for (k = 1; k < n; ++k)
-            x = x + fun(k) * fun(n - k);
-        return x;
+
     }
     public static void main(String[] args) {
         int n=5;
