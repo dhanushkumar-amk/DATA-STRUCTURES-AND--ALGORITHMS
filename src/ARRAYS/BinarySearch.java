a84e498fd08f82c230e1d2c4f8f61b2c101808f5
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-25 19:08:44 +0530

    modified

--- src/ARRAYS/BinarySearch.java
+++ src/ARRAYS/BinarySearch.java
@@ -1,6 +1,13 @@
 package ARRAYS;
 public class BinarySearch {
     public static void main(String[] args) {
-        
+        int [] arr ={7,10,12,16,22,24,32,40};
+        int l=0;
+        int r= arr.length-1;
+        int mid=r+1/2;
+        while(l<=r)
+        {
+            if(arr[])
+        }
     }
 }
