69fd9f9dd0386a38cec317fd9d0048d8630cf80d
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-25 19:51:06 +0530

    modified

--- src/ARRAYS/BinarySearch.java
+++ src/ARRAYS/BinarySearch.java
@@ -1,27 +1,22 @@
 package ARRAYS;
 public class BinarySearch {
     public static void main(String[] args) {
-
         int [] arr ={7,10};
-
         int l=0;
-
         int r= arr.length-1;
-
-
         int key =7;
-
-        while (l<r)
+        while (l<=r)
         {
           int   mid=(l+r)/2;
             if(arr[mid]==key) {
-                System.out.println(arr[mid]);
+                System.out.println(mid);
                 return;
             }
             else if(arr[mid]>key)
                 r=mid;
-            else if(arr[mid]<key) l=mid;
-                else {
+            else if(arr[mid]<key)
+                l=mid;
+            else {
                 System.out.println(-1);
                 return;
             }
