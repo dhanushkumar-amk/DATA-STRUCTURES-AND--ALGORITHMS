1b2087c60476aa17121d00108b7ea73459eb8e5f
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-25 19:59:18 +0530

    modified

--- src/ARRAYS/BinarySearch.java
+++ src/ARRAYS/BinarySearch.java
@@ -12,10 +12,12 @@ public class BinarySearch {
                 System.out.println(mid);
                 break;
             }
-            else if(arr[mid]>key)
-                r=mid-1;
-            else if(arr[mid]<key)
-                l=mid+1;
+            else if(arr[mid]>key) {
+                r = mid - 1;
+            }
+            else if(arr[mid]<key) {
+                l = mid + 1;
+            }
             else {
                 System.out.println(-1);
 
