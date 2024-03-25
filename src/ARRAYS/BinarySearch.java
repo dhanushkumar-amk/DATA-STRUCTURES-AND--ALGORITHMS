dfa9e393a7182e069c1a0119b15ff2ed90e1f4a1
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-25 19:35:41 +0530

    modified

--- src/ARRAYS/BinarySearch.java
+++ src/ARRAYS/BinarySearch.java
@@ -1,15 +1,32 @@
 package ARRAYS;
 public class BinarySearch {
     public static void main(String[] args) {
+
         int [] arr ={7,10,12,16,22,24,32,40};
+
         int l=0;
+
         int r= arr.length-1;
+
         int mid=r/2;
+
         int key =10;
 
         while (l<r)
         {
-            
+            if (key==arr[mid]) {
+                System.out.println();
+                return;
+            }
+            if(arr[mid]>key)
+            {
+                r=mid;
+                mid=r/2;
+            }
+            if(arr[mid]<key)
+            {
+
+            }
         }
     }
 }
