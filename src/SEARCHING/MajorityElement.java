b7726c47d5e57ffd1e3c804bbd0374384fe550fd
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-28 16:28:04 +0530

    modified

--- src/SEARCHING/MajorityElement.java
+++ src/SEARCHING/MajorityElement.java
@@ -1,6 +1,15 @@
 package SEARCHING;
 public class MajorityElement {
     public static void main(String[] args) {
-
+        int [] arr ={3,1,3,3,2};
+        int n= arr.length;
+        int i=0;
+        while (i<n){
+            if(arr[i]>0) {
+                int x = arr[arr[i] - 1];
+                arr[arr[i] - 1] -= 1;
+                arr[i]=x;
+            }
+        }
     }
 }
