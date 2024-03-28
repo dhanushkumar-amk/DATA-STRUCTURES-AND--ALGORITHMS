22287403317b248298ac94be7c8dd84027633873
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-28 16:07:12 +0530

    modified

--- src/SEARCHING/BinarySearchRecursive.java
+++ src/SEARCHING/BinarySearchRecursive.java
@@ -11,6 +11,23 @@ public class BinarySearchRecursive {
         else
             return  BinaryRecursive(mid+1,r,arr,key);
     }
+    static int re() {
+        long l=0;
+        long r=n-1;
+        long ans=-1;
+        for(int i=0 ;i<n; i++)
+        {
+            long mid=(l+r)/2;
+            if(arr[(int)mid]==x)
+                return(long) mid;
+            if(arr[(int)mid]<=x)
+                return (long)mid;
+            else if(arr[(int)mid]>x)
+                r=mid-1;
+            else l=mid+1;
+        }
+        return -1;
+    }
     public static void main(String[] args) {
         int [] arr = {7,10,12,16,22,24,32,40};
         int key=12;
