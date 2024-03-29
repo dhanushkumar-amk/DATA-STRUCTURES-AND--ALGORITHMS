af0d29c314e3901c2054ce9b8c2a1cf7f8d47d32
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-29 10:48:47 +0530

    modified

--- src/SEARCHING/MajorityElement.java
+++ src/SEARCHING/MajorityElement.java
@@ -1,23 +1,35 @@
 package SEARCHING;
 public class MajorityElement {
-    public static void main(String[] args) {
-        int [] arr ={3,1,3,3,2};
-        int n= arr.length;
-        int i=0;
-        while (i<n){
-            if(arr[i]>0) {
-                int x = arr[arr[i]-1];
-                arr[arr[i]] -= 1;
-                if(arr[i]==x) {
-                    arr[i] = 0;
+    static int major(int a[],int size) {
+        int ele=a[0];
+        int count=1;
+        for(int i=1; i<size; i++)
+        {
+            if(ele==a[i]){
+                count++;
+            }
+            else {
+                count--;
+                if(count<0)
+                {
+                    ele=a[i];
+                    count=1;
                 }
-                else arr[i]=x;
-            }else
-            {
-                i++;
             }
         }
-        for (int x:arr)
-            System.out.print(x+" ");
+        int coMoreOccured=0;
+        for(int i=0 ;i<size; i++)
+        {
+            if(a[i]==ele)
+                coMoreOccured++;
+        }
+        if(coMoreOccured>size/2)
+            return coMoreOccured;
+        return -1;
+    }
+    public static void main(String[] args) {
+       int [] a = {3,1,3,3,2};
+     int res=  major(a,a.length);
+        System.out.println(res);
     }
 }
