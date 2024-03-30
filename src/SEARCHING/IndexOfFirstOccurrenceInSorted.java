c0831b25598620f150a11137f474467e05ee5100
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-30 17:19:56 +0530

    modified

--- src/SEARCHING/IndexOfFirstOccurrenceInSorted.java
+++ src/SEARCHING/IndexOfFirstOccurrenceInSorted.java
@@ -1,9 +1,26 @@
 package SEARCHING;
 
 public class IndexOfFirstOccurrenceInSorted {
-
+    static int binarySearch() {
+        int [] arr ={1,10,10,10,20,20,40};
+        int n= arr.length;
+        int l=0;
+        int r=n-1;
+        for (int i=0; i<n; i++)
+        {
+            int mid=(l+r)/2;
+            if(arr[mid]==x)
+            {
+                return mid;
+            }
+            else if(arr[mid]>x)
+                r=mid-1;
+            else l=mid+1;
+        }
+    }
     public static void main(String[] args) {
-        
+     int res=  binarySearch();
+        System.out.println(res);
     }
 
 }
