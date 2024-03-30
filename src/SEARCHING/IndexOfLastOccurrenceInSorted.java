9818c2bf2a611a33ab019c6b6b46631743c77a9b
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-30 21:50:31 +0530

    modified

--- src/SEARCHING/IndexOfLastOccurrenceInSorted.java
+++ src/SEARCHING/IndexOfLastOccurrenceInSorted.java
@@ -1,27 +1,50 @@
 package SEARCHING;
 public class IndexOfLastOccurrenceInSorted {
-    static int IndexOfLastOccurrenceInSorted() {
-        int [] arr ={10,15,20,20,40,40};
+    static int firstIndex(long arr[],int n,int x) {
         int l=0;
-        int r= arr.length-1;
-        int x=40;
+        int r=n-1;
         int pos=-1;
-        while (l<=r)
+        while(l<=r)
         {
             int mid=(l+r)/2;
             if(arr[mid]==x)
             {
                 pos=mid;
-                l=mid+1;
-            }
-           else if(arr[mid]>x)
                 r=mid-1;
+            }
+            else if(arr[mid]>x)
+            {
+                r=mid-1;
+            }
+            else l=mid+1;
+        }
+        return pos;
+    }
+    static int lastIndex(long arr[],int n,int x) {
+        int l=0;
+        int r=n-1;
+        int pos=-1;
+        while(l<=r)
+        {
+            int mid=(l+r)/2;
+            if(arr[mid]==x)
+            {
+                pos=mid;
+                r=mid-1;
+            }
+            else if(arr[mid]>x)
+            {
+                r=mid-1;
+            }
             else l=mid+1;
         }
         return pos;
     }
     public static void main(String[] args) {
-        int result= IndexOfLastOccurrenceInSorted();
-        System.out.println(result);
+       long arr[]   ={1,3,5,5,5,5,7,123,125};
+        int result1 = firstIndex(arr, arr.length,7);
+        int result2 =  lastIndex(arr, arr.length, 7);
+        System.out.println(result1);
+        System.out.println(result2);
     }
 }
