d66c8c451a31fa6cbd596806d3df45a8b4ff0b2b
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-31 14:42:27 +0530

    modified

--- src/SEARCHING/SquareRoot.java
+++ src/SEARCHING/SquareRoot.java
@@ -1,6 +1,22 @@
 package SEARCHING;
 public class SquareRoot {
+    static int SquareRoot(int x) {
+        int l=0;
+        int r=x;
+        int range=x/2;
+        while (l<=r)
+        {
+            int mid=range/2;
+            if(mid*mid==x)
+             return mid;
+         else if(mid*mid>x)
+             r=mid;
+         else l=mid;
+        }
+        return -1;
+    }
     public static void main(String[] args) {
-        
+        int x=25;
+        SquareRoot(x);
     }
 }
