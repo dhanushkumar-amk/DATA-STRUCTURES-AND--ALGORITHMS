c067587b4c4eac2ca35e93480b3570e6c6da27e9
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-31 11:06:55 +0530

    modified

--- src/SEARCHING/CountOccurencesInSorted.java
+++ src/SEARCHING/CountOccurencesInSorted.java
@@ -25,7 +25,6 @@ public class CountOccurencesInSorted {
         int count=0;
         if(res==-1)
             System.out.println("0");
-        els
         for (int i=res; i<n; i++)
         {
             if(arr[i]==x)
