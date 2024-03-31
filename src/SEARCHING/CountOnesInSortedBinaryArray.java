2d1851f8a22dd286971c883280bd53810d2858b5
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-31 11:54:19 +0530

    modified

--- src/SEARCHING/CountOnesInSortedBinaryArray.java
+++ src/SEARCHING/CountOnesInSortedBinaryArray.java
@@ -1,2 +1,7 @@
-package SEARCHING;public class CountOnesInSortedBinaryArray {
+package SEARCHING;
+
+public class CountOnesInSortedBinaryArray {
+    public static void main(String[] args) {
+        
+    }
 }
