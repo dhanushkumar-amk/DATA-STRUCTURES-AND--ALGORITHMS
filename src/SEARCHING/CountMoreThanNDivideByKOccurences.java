f08cd4fd4e5f86e1bd5fdc5117953199dcba212c
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-31 11:36:34 +0530

    modified

--- src/SEARCHING/CountMoreThanNDivideByKOccurences.java
+++ src/SEARCHING/CountMoreThanNDivideByKOccurences.java
@@ -8,13 +8,13 @@ public class CountMoreThanNDivideByKOccurences {
     public static void main(String[] args) {
         HashMap<Integer,Integer> integerHashMap = new HashMap<>();
         int [] arr ={3,1,2,2,1,2,3,3};
-        for (int  x:arr)
+        for (int x:arr)
         {
-            integerHashMap.put(x,integerHashMap.getOrDefault(x,0)+1);
+            integerHashMap.put(x,integerHashMap.get(x)+1);
         }
         for (int i=0; i< arr.length; i++)
         {
-            System.out.println(integerHashMap.get(i));
+            System.out.println(arr[i]);
         }
     }
 }
