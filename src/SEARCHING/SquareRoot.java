1b4dab6b5dff5392227571499a67f5a5163b06df
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-31 14:49:30 +0530

    modified

--- src/SEARCHING/SquareRoot.java
+++ src/SEARCHING/SquareRoot.java
@@ -3,19 +3,21 @@ public class SquareRoot {
     static float SquareRoot(float x) {
         float l=0;
         float r=x/2;
+        float res=0;
         float mid=0;
         while (l<=r)
         {
              mid=(l+r)/2;
             if(mid*mid==x)
                 return mid;
-         else if(mid*mid>x)
-
-             r=mid-1;
-         else l=mid+1;
+         else if(mid*mid<x) {
+                l = mid - 1;
+                res=mid;
+            }
+         else r=mid-1;
         }
         return mid;
-    }
+    }w
     public static void main(String[] args) {
         float x=23;
      int  res= (int)SquareRoot(x);
