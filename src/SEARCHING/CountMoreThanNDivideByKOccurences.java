ef06e6376a424756be14f82ad37cae07b3812a80
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-31 11:44:11 +0530

    modified

--- src/SEARCHING/CountMoreThanNDivideByKOccurences.java
+++ src/SEARCHING/CountMoreThanNDivideByKOccurences.java
@@ -6,20 +6,17 @@ import java.util.HashMap;
 
 public class CountMoreThanNDivideByKOccurences {
     public static void main(String[] args) {
-        HashMap<Integer,Integer> integerHashMap = new HashMap<>();
-        int [] arr ={3,1,2,2,1,2,3,3};
-        int n= arr.length;
-        int k=4;
-        for (int i =0; i<n; i++)
-        {
-            integerHashMap.put(i,integerHashMap.getOrDefault(i,0)+1);
+        HashMap<Integer, Integer> integerHashMap = new HashMap<>();
+        int[] arr = {3, 1, 2, 2, 1, 2, 3, 3};
+        int n = arr.length;
+        int k = 4;
+        for (int i = 0; i < n; i++) {
+            integerHashMap.put(i, integerHashMap.getOrDefault(i, 0) + 1);
         }
-        int c=0;
-        for (int i=0; i< arr.length; i++)
-        {
-            if(integerHashMap.get(i)>n/k)
-                c++;
+        int c = 0;
+        for (int key : integerHashMap.keySet()) {
+            if (integerHashMap.get(key) > n / k)
+                System.out.println(c);
         }
-        System.out.println(c);
     }
 }
