a3835b087a41d5a6d570d863df2bbb1b5dc4858c
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-31 12:25:42 +0530

    modified

--- src/SEARCHING/CountOnesInSortedBinaryArray.java
+++ src/SEARCHING/CountOnesInSortedBinaryArray.java
@@ -12,8 +12,6 @@ public class CountOnesInSortedBinaryArray {
                 pos=mid;
                 r=mid-1;
             }
-            else if(arr[mid]>x)
-                r=mid-1;
             else l=mid+1;
         }
         return pos;
