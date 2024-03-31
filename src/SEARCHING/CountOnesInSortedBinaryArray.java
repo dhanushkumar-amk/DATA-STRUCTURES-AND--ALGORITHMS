8e1c1bae061346f674f27577dd790c14ede7a596
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-31 12:20:56 +0530

    modified

--- src/SEARCHING/CountOnesInSortedBinaryArray.java
+++ src/SEARCHING/CountOnesInSortedBinaryArray.java
@@ -24,6 +24,6 @@ public class CountOnesInSortedBinaryArray {
         int x=1;
       int first=firstAndLastIndexUsingBinarySearch(arr,n,x);
       int last=firstAndLastIndexUsingBinarySearch(arr,n,x+1);
-        System.out.println(last-first+1);
+        System.out.println(first);
     }
 }
