34472ed358ec343357d201a4e01237c8c85ca064
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-31 11:27:56 +0530

    modified

--- src/SEARCHING/CountMoreThanNDivideByKOccurences.java
+++ src/SEARCHING/CountMoreThanNDivideByKOccurences.java
@@ -14,7 +14,7 @@ public class CountMoreThanNDivideByKOccurences {
         }
         for (int i=0; i< arr.length; i++)
         {
-            System.out.println(integerHashMap.get(i));
+            System.out.println(integerHashMap.);
         }
     }
 }
