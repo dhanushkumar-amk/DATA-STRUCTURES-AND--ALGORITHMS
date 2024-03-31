45d530759a62208d595e467c7cd6a33dccb445a8
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-31 11:27:31 +0530

    modified

--- src/SEARCHING/CountMoreThanNDivideByKOccurences.java
+++ src/SEARCHING/CountMoreThanNDivideByKOccurences.java
@@ -1,6 +1,20 @@
 package SEARCHING;
+
+import INTERVAL_TREE.INTERVAL_TREE;
+
+import java.util.HashMap;
+
 public class CountMoreThanNDivideByKOccurences {
     public static void main(String[] args) {
-        
+        HashMap<Integer,Integer> integerHashMap = new HashMap<>();
+        int [] arr ={3,1,2,2,1,2,3,3};
+        for (int  x:arr)
+        {
+            integerHashMap.put(x-1,x);
+        }
+        for (int i=0; i< arr.length; i++)
+        {
+            System.out.println(integerHashMap.get(i));
+        }
     }
 }
