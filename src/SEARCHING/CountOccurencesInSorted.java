1f50894394784246246ce6936d44b0cde3573e28
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-31 10:45:41 +0530

    modified

--- src/SEARCHING/CountOccurencesInSorted.java
+++ src/SEARCHING/CountOccurencesInSorted.java
@@ -8,15 +8,14 @@ public class CountOccurencesInSorted {
         while (l<=r)
         {
             int mid=(l+r)/2;
-            if(arr[mid]==x) {
-                pos = mid;
+            if(arr[mid]>=x)
+            {
+                pos=mid;
                 r=mid-1;
             }
-            if(arr[mid]>x)
-                r=mid-1;
             else l=mid+1;
         }
-        return -1;
+        return pos;
     }
     public static void main(String[] args) {
         int [] arr ={10,20,20,20,30,30};
