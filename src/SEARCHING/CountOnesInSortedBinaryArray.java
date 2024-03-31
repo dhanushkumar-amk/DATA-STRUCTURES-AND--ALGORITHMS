47aa4a5a5c897f98ed7027f8766d71b71d99002b
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-31 12:36:40 +0530

    modified

--- src/SEARCHING/CountOnesInSortedBinaryArray.java
+++ src/SEARCHING/CountOnesInSortedBinaryArray.java
@@ -1,17 +1,37 @@
 package SEARCHING;
 
 public class CountOnesInSortedBinaryArray {
-    static int firstAndLastIndexUsingBinarySearch (int [] arr,int n,int x) {
+    static int first(int [] arr,int n,int x) {
         int l=0;
         int r=n-1;
         int pos=-1;
-        while (l<=r) {
-            int mid = (l + r) / 2;
-            if (arr[mid] >= x)
+        while(l<=r){
+            int mid=(l+r)/2;
+            if(arr[mid]==x)
             {
                 pos=mid;
                 r=mid-1;
             }
+            else if(arr[mid]>x)
+                r=mid-1;
+            else l=mid+1;
+        }
+        return pos;
+    }
+    static int last(int[] arr,int n,int x) {
+        int l=0;
+        int r=n-1;
+        int pos=1;
+        while(l<=r)
+        {
+            int mid=(l+r)/2;
+            if(arr[mid]==x)
+            {
+                pos=mid;
+                r=mid-1;
+            }
+            else if(arr[mid]>x)
+                r=mid-1;
             else l=mid+1;
         }
         return pos;
@@ -20,8 +40,8 @@ public class CountOnesInSortedBinaryArray {
         int [] arr ={0,0,0,1,1,1,1};
         int n= arr.length;
         int x=1;
-      int first=firstAndLastIndexUsingBinarySearch(arr,n,x);
-      int last=firstAndLastIndexUsingBinarySearch(arr,n,x+1)-1;
+      int first=first(arr,n,x);
+      int last=last(arr,n,x);
         int res =last-first+1;
         System.out.println(Math.abs(res));
     }
