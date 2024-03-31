e8612bf7bd807d7150d65b87d93e46355be37ea3
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-31 12:19:44 +0530

    modified

--- src/SEARCHING/CountOnesInSortedBinaryArray.java
+++ src/SEARCHING/CountOnesInSortedBinaryArray.java
@@ -9,14 +9,21 @@ public class CountOnesInSortedBinaryArray {
             int mid = (l + r) / 2;
             if (arr[mid] == x)
             {
-                
+                pos=mid;
+                r=mid-1;
             }
+            else if(arr[mid]>x)
+                r=mid-1;
+            else l=mid+1;
         }
+        return pos;
     }
     public static void main(String[] args) {
         int [] arr ={0,0,0,1,1,1,1};
-      int first=firstAndLastIndexUsingBinarySearch();
-      int last=firstAndLastIndexUsingBinarySearch();
-
+        int n= arr.length;
+        int x=1;
+      int first=firstAndLastIndexUsingBinarySearch(arr,n,x);
+      int last=firstAndLastIndexUsingBinarySearch(arr,n,x);
+        System.out.println(last-first+1);
     }
 }
