4f565f30924681a04dd9636c5de6eb590589f4bc
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-31 14:09:11 +0530

    modified

--- src/BIT_WISE/BitWiseOperator.java
+++ src/BIT_WISE/BitWiseOperator.java
@@ -2,14 +2,7 @@ package BIT_WISE;
 
 public class BitWiseOperator {
     public static void main(String[] args) {
-        /*
-         &
-         |
-         >>
-         >>>
-         <<
-         ^
-         ~
-         */
+       int x=5;//
+       x=~x;
     }
 }
