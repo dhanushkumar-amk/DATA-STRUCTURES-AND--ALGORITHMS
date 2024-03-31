666d991f265a78b2e39c6909393fcf2aeefcefb5
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-03-31 12:56:43 +0530

    modified

--- src/SEARCHING/CountOnesInSortedBinaryArray.java
+++ src/SEARCHING/CountOnesInSortedBinaryArray.java
@@ -1,29 +1,11 @@
 package SEARCHING;
 
 public class CountOnesInSortedBinaryArray {
-    static int first(int [] arr,int n,int x) {
+    static int last(int [] arr,int n,int x) {
         int l=0;
         int r=n-1;
         int pos=-1;
         while(l<=r){
-            int mid=(l+r)/2;
-            if(arr[mid]==x)
-            {
-                pos=mid;
-                r=mid-1;
-            }
-            else if(arr[mid]>x)
-                r=mid-1;
-            else l=mid+1;
-        }
-        return pos;
-    }
-    static int last(int[] arr,int n,int x) {
-        int l=0;
-        int r=n-1;
-        int pos=-1;
-        while(l<=r)
-        {
             int mid=(l+r)/2;
             if(arr[mid]==x)
             {
@@ -40,11 +22,8 @@ public class CountOnesInSortedBinaryArray {
         int [] arr ={ 0 ,0 ,0,1,1,1,1};
         int n= arr.length;
         int x=1;
-      int first=first(arr,n,x);
-      int last=last(arr,n,x);
-        System.out.println(first);
-        System.out.println(last);
-//        System.out.println(last-(first-1));
+        int l=last(arr,n,x);
+        System.out.println(l);
 
     }
 }
