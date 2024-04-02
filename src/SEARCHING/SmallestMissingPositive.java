3e42fa06b93ae97875e090ad0b0e71b15b59d0b4
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-02 12:26:55 +0530

    modified

--- src/SEARCHING/SmallestMissingPositive.java
+++ src/SEARCHING/SmallestMissingPositive.java
@@ -4,7 +4,7 @@ public class SmallestMissingPositive {
         int n=size;
 
         for (int i=0; i<n; i++) {
-            if (arr[i] > 0 || arr[i] <=n) {
+            if (arr[i] > 0 && arr[i] <=n) {
                 int x=arr[i];
                 arr[i]=arr[arr[i]-1];
                 arr[arr[i]-1]=x;
