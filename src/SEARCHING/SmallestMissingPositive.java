4889bcabd2450e5540a0d99c25a63b2d990654c0
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-02 09:07:47 +0530

    modified

--- src/SEARCHING/SmallestMissingPositive.java
+++ src/SEARCHING/SmallestMissingPositive.java
@@ -2,6 +2,6 @@ package SEARCHING;
 public class SmallestMissingPositive {
     public static void main(String[] args) {
         int [] arr ={1,2,3,5,7,8};
-        System.out.println(arr);
+        
     }
 }
