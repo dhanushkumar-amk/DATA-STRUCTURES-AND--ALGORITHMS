de6a7a196b58abc413f11db8667516659acb3dfd
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-02 14:08:16 +0530

    modified

--- src/SEARCHING/SearchInInfiniteSizedArray.java
+++ src/SEARCHING/SearchInInfiniteSizedArray.java
@@ -1,6 +1,13 @@
 package SEARCHING;
 public class SearchInInfiniteSizedArray {
-    public static void main(String[] args) {
+    static int  InfiniteSearch(int [] arr,int n,int key) {
         
+    }
+    static int binarySearch(int [] arr,int n) {
+
+    }
+    public static void main(String[] args) {
+        int arr[]  ={ 1,10,12,14,15,40,90,100,120,500};
+        InfiniteSearch(arr,arr.length,40);
     }
 }
