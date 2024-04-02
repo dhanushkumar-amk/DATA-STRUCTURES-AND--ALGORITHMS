b223e115417cb5e8343382400712ee414fbbe240
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-02 14:19:01 +0530

    modified

--- src/SEARCHING/SearchInInfiniteSizedArray.java
+++ src/SEARCHING/SearchInInfiniteSizedArray.java
@@ -5,11 +5,20 @@ public class SearchInInfiniteSizedArray {
         int r=1;
         while(r>=l){
             int mid=(r+l)/2;
-            if(arr[mid])
+            if(mid>key)
+                binarySearch(arr,n,l,mid,key);
+            else if(arr[mid]<key)
+                r=r*2;
+            else l=mid-1;
         }
+        return -1;
     }
-    static int binarySearch(int [] arr,int n) {
-
+    static int binarySearch(int [] arr,int n,int l,int r,int key) {
+        while(l<=r)
+        {
+            int mid=(l+r)/2;
+            if(arr[mid]==)
+        }
     }
     public static void main(String[] args) {
         int arr[]  ={ 1,10,12,14,15,40,90,100,120,500};
