59640e951282d45b0595b0c0581e1d1d9e05f0a9
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-02 11:02:43 +0530

    modified

--- src/SEARCHING/SmallestMissingPositive.java
+++ src/SEARCHING/SmallestMissingPositive.java
@@ -1,43 +1,21 @@
 package SEARCHING;
 public class SmallestMissingPositive {
-    static int missingNumber(int arr[], int size)
-    {
-        // Your code here
-        int ptr = 0;
-        int n=size;
-        // Check if 1 is present in array or not
-        for (int i = 0; i < n; i++) {
+    static int missingNumber(int arr[], int size) {
+        int MissingNumber = 0;
+        for (int i = 0; i < arr.length; i++) {
             if (arr[i] == 1) {
-                ptr = 1;
+                MissingNumber = 1;
                 break;
             }
         }
+        if(MissingNumber==1)
 
-        // If 1 is not present
-        if (ptr == 0)
-            return (1);
-
-        // Changing values to 1
-        for (int i = 0; i < n; i++)
-            if (arr[i] <= 0 || arr[i] > n)
-                arr[i] = 1;
-
-        // Updating indices according to values
-        for (int i = 0; i < n; i++)
-            arr[(arr[i] - 1) % n] += n;
-
-        // Finding which index has value less than n
-        for (int i = 0; i < n; i++)
-            if (arr[i] <= n)
-                return (i + 1);
-
-        // If array has values from 1 to n
-        return (n + 1);
     }
+
     public static void main(String[] args) {
-        int [] arr ={7,8,9,11,12};
-        int n= arr.length;
-     int res=   missingNumber(arr,n);
+        int[] arr = {7, 8, 9, 11, 12};
+        int n = arr.length;
+        int res = missingNumber(arr, n);
         System.out.println(res);
     }
 }
