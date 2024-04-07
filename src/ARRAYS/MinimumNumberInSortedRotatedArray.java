7bc26ed80630ce6087ab8a4dae32b890f56e9cc7
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-07 10:26:08 +0530

    modified

--- src/ARRAYS/MinimumNumberInSortedRotatedArray.java
+++ src/ARRAYS/MinimumNumberInSortedRotatedArray.java
@@ -6,7 +6,19 @@ public class MinimumNumberInSortedRotatedArray {
 
     }
     public static void main(String[] args) {
-        int [] arr ={};
-        binarySearch();
+       int [] arr ={5,6,7,8,9,10,1,2,3,4};
+       int l=0;
+       int r= arr.length-1;
+       int ans=Integer.MAX_VALUE;
+       while(l<=r) {
+           int mid=(l+r)/2;
+           if(l<=r)
+               System.out.println(mid);
+           else if(arr[l]<arr[mid] )
+           {
+               ans=Math.min(ans,arr[mid]);
+               
+           }
+       }
     }
 }
