e060679a0e59e74e6bafc9db58299766e664b96b
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-07 12:20:08 +0530

    modified

--- src/ARRAYS/MinimumNumberInSortedRotatedArray.java
+++ src/ARRAYS/MinimumNumberInSortedRotatedArray.java
@@ -2,7 +2,7 @@ package ARRAYS;
 
 public class MinimumNumberInSortedRotatedArray {
     static int binarySearch() {
-        int [] arr ={2,3,4,5,6,7,8,9,10,1};
+        int [] arr ={1,2,3,4,5};
         int l=0;
         int r= arr.length-1;
         int ans=Integer.MAX_VALUE;
