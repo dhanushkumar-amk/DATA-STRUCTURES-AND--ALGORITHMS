3c907f6ecfa09f027440a10909ef37c1358c1613
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-07 15:26:15 +0530

    modified

--- src/SEARCHING/FindPeakElementSortedArray.java
+++ src/SEARCHING/FindPeakElementSortedArray.java
@@ -4,8 +4,7 @@ public class FindPeakElementSortedArray{
     static int peak(int [] arr){
         int l=0;
         int r=arr.length-1;
-        int ans=Integer.MIN_VALUE;
-        int n= arr.length;
+
         while(l<=r){
             int mid=(l+r)/2;
             if((mid==0 || arr[mid-1]<=arr[mid]) && (mid==n-1||arr[mid+1]<=arr[mid]))
