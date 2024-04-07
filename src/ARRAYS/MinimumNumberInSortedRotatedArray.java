db0d018724dd65e9a91d0ea2b44827be4a7a00ec
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-07 09:53:17 +0530

    modified

--- src/ARRAYS/MinimumNumberInSortedRotatedArray.java
+++ src/ARRAYS/MinimumNumberInSortedRotatedArray.java
@@ -1,7 +1,12 @@
 package ARRAYS;
 
 public class MinimumNumberInSortedRotatedArray {
+    static int binarySearch() {
+        return -1;
+
+    }
     public static void main(String[] args) {
-        
+        int [] arr ={}
+        binarySearch();
     }
 }
