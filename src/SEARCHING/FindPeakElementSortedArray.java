4563d356096dc84931fad0b86299842a3b0554b1
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-07 14:36:52 +0530

    modified

--- src/SEARCHING/FindPeakElementSortedArray.java
+++ src/SEARCHING/FindPeakElementSortedArray.java
@@ -5,30 +5,7 @@ public class FindPeakElementSortedArray{
         int l=0;
         int r=arr.length-1;
         int ans=Integer.MIN_VALUE;
-        while (l<=r)
-        {
-            int mid=(l+r)/2;
-            if(mid==0) {
-                if (arr[mid] > arr[mid + 1])
-                    return arr[mid];
-            }
-            else if(mid== arr.length-1)
-            {
-                if(arr[mid]>arr[mid-1])
-                    return arr[mid];
-            }
-          else   if (arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1])
-              return   ans=Math.max(ans,arr[mid]);
-          else  if(arr[mid]>arr[mid-1]) {
-                r = mid - 1;
-                ans=Math.max(ans,arr[mid]);
-            }
-            else if(arr[mid]>arr[mid+1]) {
-                l = mid + 1;
-                ans=Math.max(ans,arr[mid]);
-            }
-        }
-        return ans;
+        
     }
     public static void main(String[] args) {
         int [] arr = {1,2,3,4,5,7,8,11,2,10};
