8495dd0c5262a26fb9aa1ab08a954529e6c22525
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-07 15:59:57 +0530

    modified

--- src/SEARCHING/TwoPointerApproach.java
+++ src/SEARCHING/TwoPointerApproach.java
@@ -4,7 +4,17 @@ public class TwoPointerApproach {
         int x = 17;
         int sum = 0;
         int [] arr ={2,5,8,12,30};
-       
+        int n= arr.length;
+        int i=0, j=n-1;
+        while (i<j){
+
+            if (arr[i] + arr[j] == x)
+                return true;
+            else if (arr[i] + arr [j] <x)i++;
+            else j--;
+        }
+
+        return false;
     }
     public static void main(String[] args) {
       int [] arr ={2,5,8,12,30};
