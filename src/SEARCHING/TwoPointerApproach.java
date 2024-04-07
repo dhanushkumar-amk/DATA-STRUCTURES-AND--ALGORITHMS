0ce6278c196da50de5817cfe01bd49c436ed25f1
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-07 15:56:38 +0530

    modified

--- src/SEARCHING/TwoPointerApproach.java
+++ src/SEARCHING/TwoPointerApproach.java
@@ -1,15 +1,26 @@
 package SEARCHING;
 public class TwoPointerApproach {
-    public static void main(String[] args) {
+    static boolean some () {
         int x = 17;
         int sum = 0;
         int [] arr ={2,5,8,12,30};
         int n=arr.length;
         for (int i = 0; i < n; i++) {
+            sum=0;
+            sum=arr[i];
             for (int j = 0; j < n; j++)
             {
-                
+                sum=0;
+                sum+=arr[j];
+                if (sum==x)
+                    return true;
+            }
         }
-      }
+        return false;
+    }
+    public static void main(String[] args) {
+      int [] arr ={2,5,8,12,30};
+     boolean res= some();
+        System.out.println(res);
     }
  }
