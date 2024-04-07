a0529ba23519854acecd69b4080fe042d4f3c3b7
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-07 14:36:32 +0530

    modified

--- src/SEARCHING/FindPeakElementSortedArray.java
+++ src/SEARCHING/FindPeakElementSortedArray.java
@@ -17,9 +17,9 @@ public class FindPeakElementSortedArray{
                 if(arr[mid]>arr[mid-1])
                     return arr[mid];
             }
-            if (arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1])
+          else   if (arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1])
               return   ans=Math.max(ans,arr[mid]);
-            if(arr[mid]>arr[mid-1]) {
+          else  if(arr[mid]>arr[mid-1]) {
                 r = mid - 1;
                 ans=Math.max(ans,arr[mid]);
             }
