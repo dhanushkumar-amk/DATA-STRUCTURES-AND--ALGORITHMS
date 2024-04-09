e79831c539df97884086dfd5803957bbe29dbed1
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-09 12:59:37 +0530

    modified

--- src/SEARCHING/FindIndexesOfsubArrayWithSum.java
+++ src/SEARCHING/FindIndexesOfsubArrayWithSum.java
@@ -27,6 +27,7 @@ public class FindIndexesOfsubArrayWithSum {
                 j++;
             }
         }
+        return arrl;
     }
     public static void main(String[] args) {
         int [] arr = {1,2,3,7,5};
