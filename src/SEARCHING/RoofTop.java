6f46f6b2b08a473f4de4229bb68ee06b02e71dc0
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-09 12:21:05 +0530

    modified

--- src/SEARCHING/RoofTop.java
+++ src/SEARCHING/RoofTop.java
@@ -6,12 +6,13 @@ public class RoofTop {
         int res=0;
         for (int i=0;i< arr.length-1; i++)
         {
+            if(arr[i+1]>arr[i])
+                c++;
           if(c>res) {
               res = Math.max(c, res);
               c=0;
           }
-            if(arr[i+1]>arr[i])
-                c++;
+
         }
         System.out.println(res);
     }
