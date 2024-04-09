35dcfe37e27c44b58e4ff2be4a575e1d6fdde9ae
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-09 15:31:57 +0530

    modified

--- src/SEARCHING/TripletInASortedArray.java
+++ src/SEARCHING/TripletInASortedArray.java
@@ -3,9 +3,6 @@ public class TripletInASortedArray {
     public static void main(String[] args) {
         int [] arr = {2,3,4,8,9,20,40};
         int n= arr.length;
-        for (int i=0; i<n; i++)
-        {
-
-        }
+        while
     }
 }
