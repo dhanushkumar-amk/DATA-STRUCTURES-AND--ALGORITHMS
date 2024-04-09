f3d721275125b6565b86f7aab6f8188d474e3b2a
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-09 12:19:03 +0530

    modified

--- src/SEARCHING/RoofTop.java
+++ src/SEARCHING/RoofTop.java
@@ -2,7 +2,7 @@ package SEARCHING;
 public class RoofTop {
     public static void main(String[] args) {
         int [] arr ={1,2,3,4};
-        int c=1;
+        int c=0;
         int res=0;
         for (int i=0;i< arr.length-1; i++)
         {
