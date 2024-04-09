9a4e1b27e6abf056c5d1f209a539ae0c6ffbb9da
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-09 15:44:18 +0530

    modified

--- src/SEARCHING/TripletInASortedArray.java
+++ src/SEARCHING/TripletInASortedArray.java
@@ -1,12 +1,12 @@
 package SEARCHING;
 public class TripletInASortedArray {
     public static void main(String[] args) {
-        int [] arr = {1,3,4,6,9,12};
+        int [] arr = {1,2,3,4,5,6};
         int n= arr.length;
         int left=0;
         int center=1;
         int right=n-1;
-        int sum=25;
+        int sum=6;
         while(left<right){
             while(center<right){
                 if (arr[left]+arr[center]+arr[right]==sum)
