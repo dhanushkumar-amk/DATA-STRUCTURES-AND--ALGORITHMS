f7390b79a6a7155649d29e0cd7575d42a695b0ec
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-09 12:05:44 +0530

    modified

--- src/SEARCHING/RoofTop.java
+++ src/SEARCHING/RoofTop.java
@@ -1,2 +1,15 @@
-package SEARCHING;public class RoofTop {
+package SEARCHING;
+public class RoofTop {
+    public static void main(String[] args) {
+        int [] arr ={1,2,2,3,2};
+        int c=0;
+        int res=Integer.MIN_VALUE;
+        for (int i=0;i< arr.length-1; i++)
+        {
+            if(arr[i+1]>arr[i])
+                c++;
+            Math.max(c,res);
+        }
+        System.out.println(res);
+    }
 }
