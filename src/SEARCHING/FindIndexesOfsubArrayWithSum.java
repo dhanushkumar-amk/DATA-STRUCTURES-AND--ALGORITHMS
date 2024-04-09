1a13276645f779453fc212bba2d7fb5d90ea967b
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-09 13:28:41 +0530

    modified

--- src/SEARCHING/FindIndexesOfsubArrayWithSum.java
+++ src/SEARCHING/FindIndexesOfsubArrayWithSum.java
@@ -5,33 +5,35 @@ import java.util.ArrayList;
 public class FindIndexesOfsubArrayWithSum {
     static ArrayList<Integer> subarraySum(int[] arr, int n, int s)
     {
-        // Your code here\
-        ArrayList<Integer> arrl = new ArrayList<Integer>();
+        ArrayList<Integer> indexes = new ArrayList<>();
         int sum=0;
         int left=0;
-        int right=0;
+        int right =0;
+
         while(right<n)
         {
-            if(arr[right]+sum ==s)
+
+            if(arr[right]+sum<s)
             {
-                arrl.add(left+1);
-                arrl.add(right+1);
-                return arrl;
-            }
-            if(arr[right]+sum<s) {
-                sum += arr[right];
+                sum+=arr[right];
                 right++;
+            }else  if(arr[right]+sum==s)
+            {
+                indexes.add(left+1);
+                indexes.add(right+1);
+                return indexes;
             }
             else {
                 sum-=arr[left];
                 left++;
             }
         }
-        arrl.add(-1);
-        return arrl;
+        indexes.add(-1);
+        indexes.add(-1);
+        return indexes;
     }
     public static void main(String[] args) {
-        int [] arr = {1,2,3,4,5,6,7,8,9,10};
-        System.out.println(subarraySum(arr, arr.length, 15));
+        int [] arr = {1,2,3,4};
+        System.out.println(subarraySum(arr, arr.length, 0));
     }
 }
