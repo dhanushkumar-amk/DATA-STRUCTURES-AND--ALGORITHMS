6eb7689aaf9b98afe7816d021db6c33352d3e39f
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-09 15:42:49 +0530

    modified

--- src/SEARCHING/TripletInASortedArray.java
+++ src/SEARCHING/TripletInASortedArray.java
@@ -11,7 +11,7 @@ public class TripletInASortedArray {
             while(center<right){
                 if (arr[left]+arr[center]+arr[right]==sum)
                 {
-                    System.out.println(left+1+" "+center+1+" "+right+1);
+                    System.out.println(arr[left]+" "+arr[center]+" "+arr[right]);
                     break;
                 }
                 center++;
