a8e62347c09b9c2bc51ab3f1e2fc8352ea040175
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-11 18:46:45 +0530

    modified

--- src/SEARCHING/RoofTop.java
+++ src/SEARCHING/RoofTop.java
@@ -14,6 +14,6 @@ public class RoofTop {
           res=Math.max(res,c);
         }
         System.out.println(res);
-        System.out.println(Integer.MAX_VALUE<<34);
+        System.out.println(Integer.MAX_VALUE<<33);
     }
 }
