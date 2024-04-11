989b3647af90e4437cd0d4e400beca724ab17b7a
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-11 21:43:19 +0530

    modified

--- src/RightShiftOperator.java
+++ src/RightShiftOperator.java
@@ -1,6 +1,6 @@
 public class RightShiftOperator {
     public static void main(String[] args) {
-          int x=-2147483648;       //
-        System.out.println(x >> 2);//
+          int x=-2147483648;       //10000000000000000000000000000000
+        System.out.println(x >> 2);//11100000000000000000000000000000
     }
 }
