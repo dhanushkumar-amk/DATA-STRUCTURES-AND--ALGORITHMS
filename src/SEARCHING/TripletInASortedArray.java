4ef4574f2db0412b86ff0a258a7dd9e6db9a1905
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-12 12:46:54 +0530

    modified

--- src/SEARCHING/TripletInASortedArray.java
+++ src/SEARCHING/TripletInASortedArray.java
@@ -1,8 +1,11 @@
 package SEARCHING;
+
+import java.util.Arrays;
+
 public class TripletInASortedArray {
     public static void main(String[] args) {
         int [] arr = {1 ,2,4,3,6};
-        
+        Arrays.sort(arr);
         int n= arr.length;
         int sum=10;
         for (int i=0; i<n-2; i++)
