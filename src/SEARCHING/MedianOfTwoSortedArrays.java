138644cc16cb8a350bcce27dd78713f82274cb06
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-12 15:29:34 +0530

    modified

--- src/SEARCHING/MedianOfTwoSortedArrays.java
+++ src/SEARCHING/MedianOfTwoSortedArrays.java
@@ -5,8 +5,7 @@ public class MedianOfTwoSortedArrays {
         int j=0;
         int ind=0;
         int [] arr3= new int[arr1.length+arr2.length];
-        int n=arr3.length;
-        while(i<arr1.length && j<arr2.length)
+        while(i<(arr1.length)/2 && j<(arr2.length/2))
         {
 
             if(arr1[i]<arr2[j])
@@ -20,25 +19,15 @@ public class MedianOfTwoSortedArrays {
                 ind++;
                 j++;
             }
-            if(ind==n/2)
-            {
-                if ((n&1)==1)
-                    return arr3[((n-1)/2)];
-                return ((double) arr3[(n - 1) / 2] + (double)arr3[((n) / 2)]) / 2;
-            }
         }
-//        while(i<arr1.length)
-//        {
-//            arr3[ind]=arr1[i];
-//            ind++;
-//            i++;
-//        }
-//        while(j<arr2.length) {
-//            arr3[ind]=arr2[j];
-//            ind++;
-//            j++;
-//        }
-//        int n=arr3.length;
+
+        int n=arr3.length;
+        if(ind==n/2)
+        {
+            if ((n&1)==1)
+                return arr3[((n-1)/2)];
+            return ((double) arr3[(n - 1) / 2] + (double)arr3[((n) / 2)]) / 2;
+        }
         return -1;
     }
     public static void main(String[] args) {
