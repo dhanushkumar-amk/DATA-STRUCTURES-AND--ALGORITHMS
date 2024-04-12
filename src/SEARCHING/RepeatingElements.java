4cbe1868d7b17c4f4ee57ccd16c21a3a6dcc1064
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-12 20:11:05 +0530

    modified

--- src/SEARCHING/RepeatingElements.java
+++ src/SEARCHING/RepeatingElements.java
@@ -1,12 +1,7 @@
 package SEARCHING;
 public class RepeatingElements {
-    public static void main(String[] args) {
-        int []arr ={1,2,2,2,3,3,3,3};
-        int ans=1;
-        for (int x:arr)
-            ans^=x;
-        int ans2=ans;
-        System.out.println(ans);
 
+    public static void main(String[] args) {
+        
     }
 }
