2b21f0936df2cc9ba7d2461f96e4b985a0c1f5a2
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-13 14:25:10 +0530

    modified

--- src/SEARCHING/TwoRepeatedElement.java
+++ src/SEARCHING/TwoRepeatedElement.java
@@ -1,10 +1,7 @@
 package SEARCHING;
 
 public class TwoRepeatedElement {
-    public static void main(String[] args) {
-        int arr[] = {1 ,2 ,1 ,3 ,4 ,3
-        };
-        int n = arr.length;
+    static int [] resss(int [] arr,int n) {
         int i = 0;
         int j=0;
         while (i < n) {
@@ -26,10 +23,41 @@ public class TwoRepeatedElement {
         }
         int [] res= new int[2];
         int z=0;
-        for (int k=0; k<n; k++)
+        for (int k=0; k<n; k++){
             if (Math.abs(arr[k])==2)
                 res[z++]=k+1;
-        for (int x:res)
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        int arr[] = {1 ,2 ,1 ,3 ,4 ,3
+        };
+        int n = arr.length;
+//        int i = 0;
+//        int j=0;
+//        while (i < n) {
+//
+//            if (arr[i] <= 0 || arr[i] > n) {
+//                i++;
+//                continue;
+//            }
+//
+//            if (arr[arr[i] - 1] > 0) {
+//                arr[i] = arr[arr[i] - 1];
+//                arr[arr[i] - 1] = -1;
+//
+//            } else {
+//                arr[arr[i] - 1]--;
+//                arr[i] = 0;
+//                i++;
+//            }
+//        }
+//        int [] res= new int[2];
+//        int z=0;
+//        for (int k=0; k<n; k++)
+//            if (Math.abs(arr[k])==2)
+//                res[z++]=k+1;
+        for (int x:resss(arr,n))
             System.out.print(x+" ");
     }
 }
