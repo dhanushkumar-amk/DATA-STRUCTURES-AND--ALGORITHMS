a9f989915dbf7977a1cda29ba307261d88071495
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-13 14:31:57 +0530

    modified

--- src/SEARCHING/TwoRepeatedElement.java
+++ src/SEARCHING/TwoRepeatedElement.java
@@ -30,8 +30,7 @@ public class TwoRepeatedElement {
         return res;
     }
     public static void main(String[] args) {
-        int arr[] = {1 ,2 ,1 ,3 ,4 ,3
-        };
+        int arr[] = {1,2,1,3,4,3};
         int n = arr.length;
         for (int x:resss(arr,n))
             System.out.print(x+" ");
