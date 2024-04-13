fb6f274ea5c3eb3e15bd785d37679ffe66f9f7b4
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-13 11:44:32 +0530

    modified

--- src/SEARCHING/SEARCHING.java
+++ src/SEARCHING/SEARCHING.java
@@ -14,22 +14,44 @@ public class SEARCHING {
     //Function to find repeated element and its frequency.
     public static Pair findRepeating(Long arr[],int n)
     {
-        long c=1;
-        long ele=0;
-        for(int i=1;i<n;i++)
+
+
+//User function Template for Java
+
+        class Solution
         {
-            if(arr[i]==arr[i-1])
-            {
-                c++;
-                ele=(long)arr[i-1];
+            // Pair Class
+            static class Pair{
+                long x;
+                long y;
+
+                Pair(long x, long y){
+                    this.x = x;
+                    this.y = y;
+                }
             }
-        }
-        if(c==0 ||ele==0)
-            return new Pair(-1,-1);
-        return new Pair(ele,c);
+            //Function to find repeated element and its frequency.
+            public static Pair findRepeating(Long arr[], int n) {
+                for (int i = 0; i < n - 1; i++) {
+                    // If current element is equal to next element, it is the repeating element
+                    if (arr[i].equals(arr[i + 1])) {
+                        // Count the frequency of the repeating element
+                        int frequency = 1;
+                        int j = i + 1;
+                        while (j < n && arr[i].equals(arr[j])) {
+                            frequency++;
+                            j++;
+                        }
+                        return new Pair(arr[i], frequency);
+                    }
+                }
+                // If no repeating element found, return {-1, -1}
+                return new Pair(-1, -1);
+            }
+        };
     }
 
     public static void main(String[] args) {
-        
+
     }
 }
