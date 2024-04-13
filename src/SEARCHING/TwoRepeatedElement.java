78ae25ef75fd16da6d6cbeb985f794144e5182a2
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-13 14:15:41 +0530

    modified

--- src/SEARCHING/TwoRepeatedElement.java
+++ src/SEARCHING/TwoRepeatedElement.java
@@ -3,25 +3,18 @@ package SEARCHING;
 public class TwoRepeatedElement {
     public static void main(String[] args) {
         int arr[] = {1,2,1,3,4,3};
-        int n = arr.length;
-        int i = 0;
         int j=0;
-        while (i < n) {
-
-            if (arr[i] <= 0 || arr[i] > n) {
-                i++;
-                continue;
+        int res[]= new int [2];
+        int freq[] = new int [n];
+        for(int i=0;i<n;i++)
+        {
+            freq[arr[i]]++;
+            if(freq[arr[i]]==2){
+                res[j]=arr[i];
+                j++;
             }
-
-            if (arr[arr[i] - 1] > 0) {
-                arr[i] = arr[arr[i] - 1];
-                arr[arr[i] - 1] = -1;
-
-            } else {
-                arr[arr[i] - 1]--;
-                arr[i] = 0;
-                i++;
-            }
+        }
+        for (int x:res) System.out.print(x+" ");
         }
     }
 }
