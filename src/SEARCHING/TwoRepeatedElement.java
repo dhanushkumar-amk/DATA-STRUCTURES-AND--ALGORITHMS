ac057c17df61debb0aa52bbd03989fe40f01658f
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-13 13:42:18 +0530

    modified

--- src/SEARCHING/TwoRepeatedElement.java
+++ src/SEARCHING/TwoRepeatedElement.java
@@ -2,30 +2,32 @@ package SEARCHING;
 
 public class TwoRepeatedElement {
     public static void main(String[] args) {
-        int arr[] = {1,2,2,1};
-        int n=arr.length;
-        int i=0;
-        while(i<n)
-        {
-            if(arr[i]<=0|| arr[i]>n)
+        int arr[] = {1, 2, 2, 1};
+        int n = arr.length;
+        int i = 0;
+        int j=0;
+        int [] arrs =new int[2];
+        while (i < n) {
+            if(arr[i]==-2)
             {
+                arrs[j++]=i+1;
+            }
+            if (arr[i] <= 0 || arr[i] > n) {
                 i++;
                 continue;
             }
 
-            if(arr[arr[i]-1]>0)
-            {
-                arr[i]=arr[arr[i]-1];
-                arr[arr[i]-1]=-1;
-            }
-            else {
-                arr[arr[i]-1]--;
-                arr[i]=0;
+            if (arr[arr[i] - 1] > 0) {
+                arr[i] = arr[arr[i] - 1];
+                arr[arr[i] - 1] = -1;
+            } else {
+                arr[arr[i] - 1]--;
+                arr[i] = 0;
                 i++;
             }
         }
-        for (int x:arr)
-            System.out.print(x+" ");
+        for (int x : arrs)
+            System.out.print(x + " ");
 //        for (int j=0;j<n; j++)
 //        {
 //            if(Math.abs(arr[j])==2)
