bafb431360e31bf844e7699b1cc746d28c902ee1
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-13 13:37:31 +0530

    modified

--- src/SEARCHING/TwoRepeatedElement.java
+++ src/SEARCHING/TwoRepeatedElement.java
@@ -2,7 +2,7 @@ package SEARCHING;
 
 public class TwoRepeatedElement {
     public static void main(String[] args) {
-        int arr[] = {1,2,1,3,4,3};
+        int arr[] = {1,2,2,1};
         int n=arr.length;
         int i=0;
         while(i<n)
