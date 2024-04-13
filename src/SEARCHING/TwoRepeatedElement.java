5d20ca4f1706037f61ec67e5a2c11df66ac07bb8
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-13 13:13:39 +0530

    modified

--- src/SEARCHING/TwoRepeatedElement.java
+++ src/SEARCHING/TwoRepeatedElement.java
@@ -1,2 +1,20 @@
-package SEARCHING;public class TwoRepeatedElement {
+package SEARCHING;
+
+public class TwoRepeatedElement {
+    public static void main(String[] args) {
+        int arr[] ={};
+        int n=arr.length;
+        int i=0;
+        for( i<n;)
+        {
+            while(arr[i]>0 && arr[i]<=n ){
+                int temp=arr[arr[i]-1];
+                if(arr[arr[i]-1]<0){
+                    arr[arr[i]-1]-=1;
+                }
+                else arr[arr[i]-1]=-1;
+            }
+            i++;
+        }
+    }
 }
