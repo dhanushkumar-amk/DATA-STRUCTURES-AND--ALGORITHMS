8e70274f102117030d6e555938894e2d02323978
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-15 20:56:03 +0530

    modified

--- src/SEARCHING/AllocateMinimumPages.java
+++ src/SEARCHING/AllocateMinimumPages.java
@@ -11,6 +11,16 @@ public class AllocateMinimumPages {
         Arrays.fill(arr, 32); // Fill the array with a constant value for simplicity
 
         // Measure the time taken by the first solution
+
+        // Measure the time taken by the second solution
+        long startTime2 = System.nanoTime();
+        int sum2 = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum2 += arr[i];
+        }
+        long endTime2 = System.nanoTime();
+        double elapsedTime2 = (endTime2 - startTime2) / 1_000_000.0; // Convert nanoseconds to milliseconds
+        System.out.println("Time taken by second solution: " + elapsedTime2 + " milliseconds");
         long startTime1 = System.nanoTime();
         long sum=0;
         int left=0;
@@ -25,13 +35,5 @@ public class AllocateMinimumPages {
         double elapsedTime1 = (endTime1 - startTime1) / 1_000_000.0; // Convert nanoseconds to milliseconds
         System.out.println("Time taken by first solution: " + elapsedTime1 + " milliseconds");
 
-        // Measure the time taken by the second solution
-        long startTime2 = System.nanoTime();
-        int sum2 = 0;
-        for (int i = 0; i < arr.length; i++) {
-            sum2 += arr[i];
-        }
-        long endTime2 = System.nanoTime();
-        double elapsedTime2 = (endTime2 - startTime2) / 1_000_000.0; // Convert nanoseconds to milliseconds
-        System.out.println("Time taken by second solution: " + elapsedTime2 + " milliseconds");}
+    }
 }
