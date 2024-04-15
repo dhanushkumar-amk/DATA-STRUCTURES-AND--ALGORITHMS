20c8406f066139ec519a32b8224488f4929993d0
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-15 20:33:56 +0530

    modified

--- src/SEARCHING/AllocateMinimumPages.java
+++ src/SEARCHING/AllocateMinimumPages.java
@@ -8,5 +8,9 @@ public class AllocateMinimumPages {
         int [] arr ={12,34,67,90};
         int res= recursive(arr, arr.length,4);
         System.out.println(res);
+        int left=0;
+        int leftMid= arr.length/3;
+        int right= arr.length;
+        System.out.println(leftMid );
     }
 }
