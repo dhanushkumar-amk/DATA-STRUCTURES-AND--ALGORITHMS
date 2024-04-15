cc66fe79ca740ef900086e28b08ab4197022aa22
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-15 20:39:28 +0530

    modified

--- src/SEARCHING/AllocateMinimumPages.java
+++ src/SEARCHING/AllocateMinimumPages.java
@@ -1,13 +1,12 @@
 package SEARCHING;
 
 public class AllocateMinimumPages {
-    static int recursive(int [] arr,int n,int student){
-return 1;
-    }
+//    static int recursive(int [] arr,int n,int student){
+//return 1;
+//    }
     public static void main(String[] args) {
         int [] arr ={12,34,67,90};
-        int res= recursive(arr, arr.length,4);
-//        System.out.println(res);
+
         int left=0;
         int leftMid= (arr.length/2)-1;
         int rightMid=( arr.length/2);
