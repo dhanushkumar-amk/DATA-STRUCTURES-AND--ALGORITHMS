0aa51c9d8cc7de6ef9b9e48f8a1748b43398e0a7
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-16 17:08:06 +0530

    modified

--- src/SEARCHING/AllocateMinimumPages.java
+++ src/SEARCHING/AllocateMinimumPages.java
@@ -1,7 +1,7 @@
 package SEARCHING;
 
 public class AllocateMinimumPages {
-    static int binarySearch(int [] arr,int n) {
+    static int binarySearch(int [] arr,int n,int students) {
         int left=0;
         int right=n-1;
         int Min=Integer.MAX_VALUE,max=Integer.MIN_VALUE;
@@ -17,20 +17,24 @@ public class AllocateMinimumPages {
         while(l<=r)
         {
             int mid=(l+r)/2;
-            if(isPossible(arr,n,mid))
+            if(isPossible(arr,n,mid,students))
             {
 
             }
         }
         return -1;
     }
-    static int isPossible(int[] arr,int n,int mid) {
-        for (int i=0;)
+    static int isPossible(int[] arr,int n,int mid,int students) {
+        for (int i=0;i<n;i++)
+        {
+            
+        }
     }
 
     public static void main(String[] args) {
         int [] arr ={12,34,67,90};
-        int res=binarySearch(arr, arr.length);
+        int students=2;
+        int res=binarySearch(arr, arr.length,students);
         System.out.println(res);
     }
 }
