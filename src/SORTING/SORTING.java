c0c82188ba3ecd41afaa2ab718620a3967da81ef
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-17 20:06:44 +0530

    modified

--- src/SORTING/SORTING.java
+++ src/SORTING/SORTING.java
@@ -6,6 +6,6 @@ class  SORTING {
     public static void main(String[] args) {
         char [] arr ={'A','a'};
         Arrays.sort(arr);
-        for (int x:arr) System.out.print(x);
+        for (char x:arr) System.out.print(x);
     }
 }
