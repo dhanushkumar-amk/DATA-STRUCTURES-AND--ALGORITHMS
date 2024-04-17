4874bfde498b37fcddd806047b2ea7ea28814be4
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-17 14:53:50 +0530

    modified

--- src/SEARCHING/ToggleTheMiddle.java
+++ src/SEARCHING/ToggleTheMiddle.java
@@ -2,16 +2,15 @@ package SEARCHING;
 
 public class ToggleTheMiddle {
     static String str="";
-    static void     recursion(int n,String str) {
+    static void     recursion(int n) {
         if(n==0)
             return ;
-        recursion(n/2,str);
+        recursion(n/2);
         str+=n%2;
-
     }
     public static void main(String[] args) {
         int n=5;
-       recursion(n,str);
+       recursion(n);
         System.out.println(str);
     }
 }
