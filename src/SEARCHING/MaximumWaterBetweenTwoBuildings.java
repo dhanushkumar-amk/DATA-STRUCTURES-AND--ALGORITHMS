fdc1d665719275d335eeb18feaaaad7c2d0f934a
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-17 10:55:04 +0530

    modified

--- src/SEARCHING/MaximumWaterBetweenTwoBuildings.java
+++ src/SEARCHING/MaximumWaterBetweenTwoBuildings.java
@@ -7,6 +7,12 @@ public class MaximumWaterBetweenTwoBuildings {
         int right=n-1;
         int sum=0;
         int max=0;
-        
+        while(left<right)
+        {
+            max=Math.max((left+right)*Math.min(left,right),max);
+                left++;
+                right--;
+        }
+        System.out.println(max);
     }
 }
