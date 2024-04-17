09f7b953d79b96fbbd5d046c31d62666f0d219e2
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-17 20:06:24 +0530

    modified

--- src/SORTING/SORTING.java
+++ src/SORTING/SORTING.java
@@ -1,7 +1,11 @@
 package SORTING;
 
+import java.util.Arrays;
+
 class  SORTING {
     public static void main(String[] args) {
-        int [] arr ={1,2,4,3};
+        char [] arr ={'A','a'};
+        Arrays.sort(arr);
+        for (int x:arr) System.out.print(x+" ");
     }
 }
