c330e37cb77e838789a39b5155440c75842a389c
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-17 15:04:31 +0530

    modified

--- src/SEARCHING/ToggleTheMiddle.java
+++ src/SEARCHING/ToggleTheMiddle.java
@@ -12,5 +12,19 @@ public class ToggleTheMiddle {
         int n=5;
        recursion(n);
         System.out.println(str);
+        int len=str.length();
+        int left=0;
+        int right=len;
+        String strs="";
+        for (int i=0; i<n; i++) {
+            if ((len & 1) == 1 && i + 1 == len / 2)
+                strs += '1';
+            else if ((len & 1) == 0 && (i == len / 2) || (i + 1 == len / 2))
+                strs += "1";
+            else {
+                strs += n % 2;
+            }
+        }
+        System.out.println(strs);
     }
 }
