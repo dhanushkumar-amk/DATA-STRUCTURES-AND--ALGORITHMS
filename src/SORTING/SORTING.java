913aedac300d40d173e31f84f4829e489aee0ee9
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-18 16:33:01 +0530

    modified

--- src/SORTING/SORTING.java
+++ src/SORTING/SORTING.java
@@ -3,9 +3,9 @@ class  SORTING {
     public static void main(String[] args) {
         int [] arr ={23,432,5,42};
         int n= arr.length;
-        for (int i=0; i<n-1; i++)
+        for (int i=0; i<n-2; i++)
         {
-            for (int j=0; j<n-i-1;j++)
+            for (int j=0; j<n-i;j++)
             {
                 if(arr[i]>arr[j])
                 {
