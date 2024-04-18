c6c39344b043295c5050eac1b8ec41297753f571
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-18 16:48:13 +0530

    modified

--- src/SORTING/SORTING.java
+++ src/SORTING/SORTING.java
@@ -5,9 +5,9 @@ class  SORTING {
         int n= arr.length;
         for (int i=0; i<n-1; i++)
         {
-            for (int j=0; j<n-1-i;j++)
+            for (int j=0; j<n-1-i; j++)
             {
-                if(arr[j]>arr[i])
+                if(arr[j]>arr[j+1])
                 {
                     int temp=arr[j];
                     arr[j]=arr[i];
