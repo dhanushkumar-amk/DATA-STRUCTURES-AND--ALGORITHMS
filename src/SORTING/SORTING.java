cf8e7b8bdfd472230f6fc7e547654e6a9017fb49
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-18 13:00:29 +0530

    modified

--- src/SORTING/SORTING.java
+++ src/SORTING/SORTING.java
@@ -1,7 +1,12 @@
 package SORTING;
-
 class  SORTING {
     public static void main(String[] args) {
-        
+        int [] arr ={7,3,5,9,2};
+        int left=0;
+        int right=arr.length;
+        while(left<right)
+        {
+           if(arr[left])
+        }
     }
 }
