4f2f52e4f46acb24767fa994fd4b468d465fa54a
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-18 16:32:46 +0530

    modified

--- src/SORTING/SORTING.java
+++ src/SORTING/SORTING.java
@@ -5,7 +5,16 @@ class  SORTING {
         int n= arr.length;
         for (int i=0; i<n-1; i++)
         {
-            for (int j=0; j<n ;)
+            for (int j=0; j<n-i;j++)
+            {
+                if(arr[i]>arr[j])
+                {
+                    int temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
         }
+        for (int x:arr) System.out.print(x+" ");
     }
 }
