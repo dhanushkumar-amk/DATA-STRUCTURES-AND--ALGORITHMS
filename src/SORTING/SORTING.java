2ab6b47514c73c8f6ffd3d1ea9d3fa5b9cf240ad
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-18 13:07:03 +0530

    modified

--- src/SORTING/SORTING.java
+++ src/SORTING/SORTING.java
@@ -2,11 +2,21 @@ package SORTING;
 class  SORTING {
     public static void main(String[] args) {
         int [] arr ={7,3,5,9,2};
-        int left=0;
-        int right=arr.length;
-        while(left<right)
+        int n= arr.length;
+        for (int i=0; i<n; i++)
         {
-           if(arr[left])
+            for (int j=i+1;j<n; j++ )
+            {
+                if(j==n-1)
+                    n--;
+                if(arr[i]>arr[j]){
+                    int temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
         }
+        for (int temp:arr)
+            System.out.print(temp );
     }
 }
