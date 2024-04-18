bd1ef0407ed66e8bc12d64a3cdcee1bb5a80e7cc
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-18 16:47:26 +0530

    modified

--- src/SORTING/SORTING.java
+++ src/SORTING/SORTING.java
@@ -9,9 +9,9 @@ class  SORTING {
             {
                 if(arr[j]>arr[i])
                 {
-                    int temp=arr[i];
-                    arr[i]=arr[j];
-                    arr[j]=temp;
+                    int temp=arr[j];
+                    arr[j]=arr[i];
+                    arr[i]=temp;
                 }
             }
         }
