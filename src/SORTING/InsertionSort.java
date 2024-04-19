7a78aea8319cad15bc929531373f676382d1de96
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-19 13:53:54 +0530

    modified

--- src/SORTING/InsertionSort.java
+++ src/SORTING/InsertionSort.java
@@ -5,7 +5,14 @@ public class InsertionSort {
     static void insert(int []arr,int n) {
         for (int i=1; i<n;i++)
         {
-            
+            int temp=arr[i];
+            int j=i-1;
+            while(j>=0 && arr[i]<arr[j])
+            {
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j]=temp;
         }
     }
     public static void main(String[] args) {
