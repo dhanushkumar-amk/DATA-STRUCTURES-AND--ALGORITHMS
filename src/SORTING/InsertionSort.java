3458565decd342e74d03dc560b52017d5a2ccfba
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-19 13:43:58 +0530

    modified

--- src/SORTING/InsertionSort.java
+++ src/SORTING/InsertionSort.java
@@ -25,7 +25,7 @@ public class InsertionSort {
         }
     }
     public static void main(String[] args) {
-        int [] arr ={1,2,23,32,4345343};
+        int [] arr ={1,2,3,4,};
         insert(arr, arr.length);
         for (int x:arr) System.out.print(x+" ");
     }
