39604090f338595ee2a2227d559217ea807dc8bb
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-19 13:17:36 +0530

    modified

--- src/SORTING/InsertionSort.java
+++ src/SORTING/InsertionSort.java
@@ -1,2 +1,17 @@
-package SORTING;public class InsertionSort {
+package SORTING;
+
+public class InsertionSort {
+    static void shift(int [] arr,int n,int i,int j) {
+
+    }
+    static void insert(int []arr,int n) {
+        for (int i=0; i<n;i++)
+        {
+
+        }
+    }
+    public static void main(String[] args) {
+        int [] arr ={}
+        insert();
+    }
 }
