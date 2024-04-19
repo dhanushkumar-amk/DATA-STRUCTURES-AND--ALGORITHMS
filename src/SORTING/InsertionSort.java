1305996638d4d0a7051ad28339795804270e7095
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-19 13:50:32 +0530

    modified

--- src/SORTING/InsertionSort.java
+++ src/SORTING/InsertionSort.java
@@ -1,27 +1,11 @@
 package SORTING;
 
 public class InsertionSort {
-    static void shift(int [] arr,int n,int end,int start) {
-        for (int i=end;i>start;i--)
-        {
-            arr[i]=arr[i-1];
-        }
-    }
+
     static void insert(int []arr,int n) {
         for (int i=1; i<n;i++)
         {
-            if(arr[i]<arr[i-1])
-            {
-                int min=arr[i-1];
-                int temp=arr[i];
-                for (int j=i;j>=0 ;j--)
-                {
-                    if(arr[i]<arr[j])
-                        min=j;
-                }
-                shift(arr,n,i,min);
-                arr[min]=temp;
-            }
+            
         }
     }
     public static void main(String[] args) {
