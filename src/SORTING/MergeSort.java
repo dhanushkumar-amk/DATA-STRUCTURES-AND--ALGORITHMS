582ae0b6f216368c64e5b50d41ebccedcf625045
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-20 17:12:57 +0530

    modified

--- src/SORTING/MergeSort.java
+++ src/SORTING/MergeSort.java
@@ -51,7 +51,7 @@ public class MergeSort {
 
         // Display a message indicating successful creation
         System.out.println("Array of size " + size + " with unique numbers created successfully.");
-        mergeSort(largeArray,0, largeArray.length-1);
+//        mergeSort(largeArray,0, largeArray.length-1);
         for (int x:largeArray) System.out.println(x+" ");
     }
     private static void shuffleArray(int[] array) {
