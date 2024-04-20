0cd80ec8b5fcb1900e14b98d8588ca66f07db957
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-20 18:52:15 +0530

    modified

--- src/SORTING/MergeSortedArraysWithoutExtraSpace.java
+++ src/SORTING/MergeSortedArraysWithoutExtraSpace.java
@@ -21,13 +21,14 @@ public class MergeSortedArraysWithoutExtraSpace {
         int arr2[]={3,4,5,28,37,42,43,46};
         int lastPointer= arr1.length-1;
         int firstPointer= 0;
-        while(arr1[lastPointer]>arr2[firstPointer] ) {
-            int temp=arr2[firstPointer];
-            arr2[firstPointer]=arr1[lastPointer];
-            arr1[lastPointer]=temp;
+        while(lastPointer >= 0 && arr1[lastPointer] > arr2[firstPointer]) {
+            int temp = arr2[firstPointer];
+            arr2[firstPointer] = arr1[lastPointer];
+            arr1[lastPointer] = temp;
             lastPointer--;
             firstPointer++;
         }
+
         insertionSort(arr1, arr1.length);
         insertionSort(arr2, arr2.length);
         print(arr1);
