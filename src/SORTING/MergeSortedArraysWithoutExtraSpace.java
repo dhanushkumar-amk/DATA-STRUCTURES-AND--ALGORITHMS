f9630ee493940a6f78a1ef59588d2d47b2ed5ece
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-20 18:41:17 +0530

    modified

--- src/SORTING/MergeSortedArraysWithoutExtraSpace.java
+++ src/SORTING/MergeSortedArraysWithoutExtraSpace.java
@@ -1,6 +1,21 @@
 package SORTING;
 public class MergeSortedArraysWithoutExtraSpace {
-
+    static void insertionSort(int arr[],int n) {
+        for (int i=1; i< n;i++)
+        {
+            if(arr[i-1]>arr[i])
+            {
+                int key=arr[i];
+                int j=i-1;
+                while(j>=0 && arr[j]>key)
+                    arr[j+1]=arr[j--];
+               arr [j+1]=key;
+            }
+        }
+    }
+    static void print(int [] arr) {
+        for (int x:arr) System.out.print(x+" ");
+    }
     public static void main(String[] args) {
         int left[] = {1,4,8,12};
         int right[]={2,3,5,7,9};
@@ -13,17 +28,9 @@ public class MergeSortedArraysWithoutExtraSpace {
             lastPointer--;
             firstPointer++;
         }
-        for (int i=1; i< left.length;i++)
-        {
-            if(left[i-1]>left[i])
-            {
-                int key=left[i];
-                int j=i-1;
-                while(j>=0 && left[j]>key)
-                    left[j+1]=left[j--];
-                left[j+1]=key;
-            }
-        }
-        for (int x:left) System.out.print(x+" ");
+        insertionSort(left, left.length);
+        insertionSort(right, right.length);
+        print(left);
+        print(right);
     }
 }
