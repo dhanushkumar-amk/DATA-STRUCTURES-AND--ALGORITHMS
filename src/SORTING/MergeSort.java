462a466de18d5af17964bd66061bc68760bc3464
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-20 13:47:39 +0530

    modified

--- src/SORTING/MergeSort.java
+++ src/SORTING/MergeSort.java
@@ -21,7 +21,7 @@ public class MergeSort {
             int mid = (l + r) / 2;
             mergeSort(arr, l, mid);
             mergeSort(arr, mid + 1, r);
-            shift(arr, arr.length, l, r);
+            shift(arr, arr.length, l, r-mid);
         }
     }
     public static void main(String[] args) {
