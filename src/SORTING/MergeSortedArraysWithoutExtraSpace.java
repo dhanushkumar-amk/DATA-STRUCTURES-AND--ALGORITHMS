6b7683b7585ec56a29f3113cd3698554d6025ed0
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-20 18:04:33 +0530

    modified

--- src/SORTING/MergeSortedArraysWithoutExtraSpace.java
+++ src/SORTING/MergeSortedArraysWithoutExtraSpace.java
@@ -1,2 +1,9 @@
-package SORTING;public class MergeSortedArraysWithoutExtraSpace {
+package SORTING;
+public class MergeSortedArraysWithoutExtraSpace {
+
+    public static void main(String[] args) {
+        int left[] = {1,3,5,7};
+        int right[]={7,5,6,8,9};
+
+    }
 }
