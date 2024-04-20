ca9fd67f632b7887fa57d54b4b856fe447da64f3
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-20 18:28:14 +0530

    modified

--- src/SORTING/MergeSortedArraysWithoutExtraSpace.java
+++ src/SORTING/MergeSortedArraysWithoutExtraSpace.java
@@ -4,8 +4,8 @@ public class MergeSortedArraysWithoutExtraSpace {
     public static void main(String[] args) {
         int left[] = {1,4,8,12};
         int right[]={2,3,5,7,9};
-        int lastPointer= left.length;;
-        int firstPointer= right.length;
+        int lastPointer= left.length-1;
+        int firstPointer= 0;
         while(left[lastPointer]>right[firstPointer]) {
             int temp=right[firstPointer];
             right[firstPointer]=left[lastPointer];
