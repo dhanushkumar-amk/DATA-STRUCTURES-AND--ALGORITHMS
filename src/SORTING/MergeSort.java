808ce16f9c60fae7ea92ad066b507764a060015d
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-20 13:35:44 +0530

    modified

--- src/SORTING/MergeSort.java
+++ src/SORTING/MergeSort.java
@@ -1,7 +1,31 @@
 package SORTING;
-
 public class MergeSort {
+    static int [] dup = new int[5];
+    static int k=0;
+   static void shift(int [] arr,int n,int i,int j) {
+       while(i<n && j<n)
+       {
+           if(arr[i]<arr[j])
+               dup[k++]=arr[i++];
+           else
+               dup[k++]=arr[j++];
+       }
+       while(i<n)
+           dup[k++]=arr[i++];
+       while(j<n)
+           dup[k++]=arr[j++];
+    }
+    static void mergeSort(int [] arr,int l,int r) {
+        if(l<r)
+            return ;
+        int mid=(l+r)/2;
+        mergeSort(arr,l,mid);
+        mergeSort(arr,mid+1,r);
+        shift(arr,arr.length,l,r);
+    }
     public static void main(String[] args) {
-
+        int [] arr ={5,4,3,2,1};
+        mergeSort(arr,0, arr.length);
+        for (int x :arr) System.out.print(x+" ");
     }
 }
