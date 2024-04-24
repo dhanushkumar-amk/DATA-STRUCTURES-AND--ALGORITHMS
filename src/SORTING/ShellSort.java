75134150e57e148db0b4d0cbea5c20d271cffa7f
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-24 14:47:18 +0530

    modified

--- src/SORTING/ShellSort.java
+++ src/SORTING/ShellSort.java
@@ -3,6 +3,7 @@ public class ShellSort {
     public static void main(String[] args) {
         int [] arr1 ={1,3,5,7};
         int [] arr2 ={0,2,6,8,9};
-        
+        int gap=(arr1.length/2)+(arr2.length/2)/2;
+        System.out.println(gap);
     }
 }
