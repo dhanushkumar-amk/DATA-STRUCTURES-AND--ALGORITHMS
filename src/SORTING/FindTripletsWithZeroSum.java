d3c7fc2a4d1c573215582783a148dc7d92e7a6d4
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-24 20:31:47 +0530

    modified

--- src/SORTING/FindTripletsWithZeroSum.java
+++ src/SORTING/FindTripletsWithZeroSum.java
@@ -4,25 +4,10 @@ public class FindTripletsWithZeroSum {
     public static void main(String[] args) {
         int [] arr ={1, 2, 3};
         int n= arr.length;
-        int i=0;
-        int j=i+1;
-        int k=n-1;
-        int sum=0;
-        while(j<k &&  i<k-1)
+        for (int i=0; i<n ;i++)
         {
-            if(arr[i]+arr[j]+arr[k]==0)
-            {
-                System.out.println(1);
-                break;
-            }
-            else j++;
-
-            if(j>=k)
-            {
-                i++;
-                j=i+1;
-                k--;
-            }
+            if(i>0 && arr[i]==arr[i-1])continue;
+            
         }
     }
 }
