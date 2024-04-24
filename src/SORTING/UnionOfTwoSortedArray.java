aaba98eb83c29808a804c22f5ec1156305d2de6a
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-24 23:03:56 +0530

    modified

--- src/SORTING/UnionOfTwoSortedArray.java
+++ src/SORTING/UnionOfTwoSortedArray.java
@@ -30,12 +30,12 @@ public class UnionOfTwoSortedArray {
                 j++;
             }
         }
-//        while (i < len1)
-//            if (i == 0 || arr1[i] != arr1[i - 1])
-//                ans[k++] = arr1[i++];
-//        while (j < len2)
-//            if (j == 0 || arr2[j] != arr2[j - 1])
-//                ans[k++] = arr2[j++];
+        while (i < len1)
+            if (i == 0 || arr1[i] != arr1[i - 1])
+                ans[k++] = arr1[i++];
+        while (j < len2)
+            if (j == 0 || arr2[j] != arr2[j - 1])
+                ans[k++] = arr2[j++];
 
 
         for (int x:ans) System.out.print(x+" ");
