90bbd998f0fbdc5a12eba1f0f4104bec327d4362
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-24 21:53:44 +0530

    modified

--- src/SORTING/UnionOfTwoSortedArray.java
+++ src/SORTING/UnionOfTwoSortedArray.java
@@ -1,7 +1,17 @@
 package SORTING;
 
+import java.util.ArrayList;
+import java.util.TreeSet;
+
 public class UnionOfTwoSortedArray {
     public static void main(String[] args) {
+        int [] arr1 = {1,2,3,4,5};
+        int [] arr2 = {1,2,3,6,7};
+        TreeSet<Integer> setOne= new TreeSet();
+        TreeSet<Integer> setTwo= new TreeSet();
+        ArrayList<Integer> list= new ArrayList();
+        for(int x:arr1)setOne.add(x);
+        for(int y:arr2)setTwo.add(y);
         
     }
 }
