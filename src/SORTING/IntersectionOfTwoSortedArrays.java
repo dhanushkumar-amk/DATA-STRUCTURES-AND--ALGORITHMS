6a63f7781656fb2655657daed6ecd6ddd0e895ba
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-24 21:01:57 +0530

    modified

--- src/SORTING/IntersectionOfTwoSortedArrays.java
+++ src/SORTING/IntersectionOfTwoSortedArrays.java
@@ -1,8 +1,10 @@
 package SORTING;
 
 public class IntersectionOfTwoSortedArrays {
+    
     public static void main(String[] args) {
         int arr1[] = {1, 2, 3, 4};
-        
+        int arr2 [] = {2, 4, 6, 7, 8};
+
     }
 }
