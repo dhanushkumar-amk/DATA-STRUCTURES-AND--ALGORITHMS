f85eeabc259257d57ea7035ffabb9c41ea7ab736
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-24 21:59:37 +0530

    modified

--- src/SORTING/UnionOfTwoSortedArray.java
+++ src/SORTING/UnionOfTwoSortedArray.java
@@ -4,6 +4,17 @@ import java.util.ArrayList;
 import java.util.TreeSet;
 
 public class UnionOfTwoSortedArray {
+    static int search(TreeSet<Integer> x,int key) {
+        int l=0;
+        int r=x.size()-1;
+        while(l<=r)
+        {
+            int mid=(l+r)/2;
+            if()
+        }
+        return 0;
+    }
+
     public static void main(String[] args) {
         int [] arr1 = {1,2,3,4,5};
         int [] arr2 = {1,2,3,6,7};
@@ -12,6 +23,6 @@ public class UnionOfTwoSortedArray {
         ArrayList<Integer> list= new ArrayList();
         for(int x:arr1)setOne.add(x);
         for(int y:arr2)setTwo.add(y);
-        
+
     }
 }
