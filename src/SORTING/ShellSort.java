0d89dccae588f9e3ef6cd20cceac3f8559b8bfa2
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-24 15:29:55 +0530

    modified

--- src/SORTING/ShellSort.java
+++ src/SORTING/ShellSort.java
@@ -8,7 +8,7 @@ public class ShellSort {
     public static void main(String[] args) {
         int [] arr1 ={1,3,5,7};
         int [] arr2 ={0,2,6,8,9};
-        int gap=(arr1.length/2)+(arr2.length/2);
+        int gap=(arr1.length+arr2.length)/2+(arr1.length+arr2.length)%2;
         int len=arr1.length+arr2.length;
         int len1=arr1.length;
         int len2=arr2.length;
