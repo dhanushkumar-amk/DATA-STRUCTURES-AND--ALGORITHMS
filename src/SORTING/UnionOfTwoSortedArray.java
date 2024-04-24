83e1e98a6e91c1d475f4e7255f2013a1990138ee
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-24 22:58:56 +0530

    modified

--- src/SORTING/UnionOfTwoSortedArray.java
+++ src/SORTING/UnionOfTwoSortedArray.java
@@ -26,7 +26,6 @@ public class UnionOfTwoSortedArray {
             else if(arr1[i]>arr2[j])
                 ans[k++]=arr2[j++];
             else {
-                ans[k++]=arr1[i];
                 i++;
                 j++;
             }
