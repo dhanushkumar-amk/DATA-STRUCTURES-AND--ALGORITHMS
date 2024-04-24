5e2919b3667d058f6b8ae28be135aa1c3d08bd7e
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-24 14:07:03 +0530

    modified

--- src/SORTING/ShellSort.java
+++ src/SORTING/ShellSort.java
@@ -1,11 +1,6 @@
 package SORTING;
 public class ShellSort {
     public static void main(String[] args) {
-        int [] arr= {50,40,30,20,10,5,2,1};
-        int gap= arr.length;
-        for (int i=gap/2; i>0; i/=2)
-        {
-            for (int )
-        }
+
     }
 }
