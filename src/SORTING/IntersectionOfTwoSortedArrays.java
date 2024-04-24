08d579d9ca82eb8d1c195f25b59c20dadea5c5b0
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-24 21:23:41 +0530

    modified

--- src/SORTING/IntersectionOfTwoSortedArrays.java
+++ src/SORTING/IntersectionOfTwoSortedArrays.java
@@ -1,43 +1,47 @@
-package SORTING;
+package  SORTING;
 
 import java.util.ArrayList;
 
-public class IntersectionOfTwoSortedArrays {
-    static int bs(int arr2[],int key) {
-        int l=0;
-        int r=arr2.length-1;
+class IntersectionOfTwoSortedArrays {
+    static int  binarySearch(int arr[],int key) {
+        int l =0;
+        int  r = arr.length-1;
         while(l<=r)
         {
             int mid=(l+r)/2;
 
-            if(arr2[mid]==key)
+            if(arr[mid]==key)
                 return mid;
-            else if(arr2[mid]<key)
+            else if(arr[mid]<key)
                 l=mid+1;
             else r=mid-1;
         }
         return -1;
     }
-    static ArrayList<Integer> printIntersection(int arr1[], int arr2[], int n, int m)
-    {
-        // add your code here
-        ArrayList<Integer> arrayList = new ArrayList<Integer>();
-        for(int i=0; i<n; i++)
+    static int[] intersection(int[] nums1, int[] nums2) {
+        ArrayList<Integer> lis = new ArrayList<Integer> ();
+
+        int j=0;
+        for (int i=0; i<nums1.length; i++)
         {
-            if(i>0 && arr1[i]==arr1[i-1])
+            if(i>0 && nums1[i]==nums1[i-1])
                 continue;
-            int ind=bs(arr2,arr1[i]);
-            if(!(ind==-1)){
-                if(arr1[i]==arr2[ind])
-                    arrayList.add(arr1[i]);
-            }
+            int ind= binarySearch(nums2,nums1[i]);
+            if(ind==-1)
+                continue;
+
+            if(nums1[i]==nums2[ind])
+                lis.add(nums1[i]);
         }
-        return arrayList;
+        int [] ans =  new int [lis.size()];
+        for(int i=0; i<lis.size(); i++)
+            ans[i]=lis.get(i);
+        return ans;
     }
+
     public static void main(String[] args) {
-        int arr1[] = {1, 2, 3, 4};
-        int arr2 [] = {2, 4, 6, 7, 8};
-      ArrayList<Integer> arrayList =  printIntersection(arr1,arr2,arr1.length,arr2.length);
-        System.out.println(arrayList);
+        int nums1[] ={4,9,5};
+        int nums2[]={9,4,9,8,4};
+        intersection(nums1,nums2);
     }
 }
