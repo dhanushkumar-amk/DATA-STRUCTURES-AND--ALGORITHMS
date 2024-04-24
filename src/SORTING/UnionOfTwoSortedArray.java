9d789aec9bafeb5ab2a54c04e117aa1f389262df
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-24 22:37:11 +0530

    modified

--- src/SORTING/UnionOfTwoSortedArray.java
+++ src/SORTING/UnionOfTwoSortedArray.java
@@ -2,11 +2,9 @@ package SORTING;
 
 public class UnionOfTwoSortedArray {
 
-
-
     public static void main(String[] args) {
-        int [] arr1 = {1,2,3,4,5};
+        int [] arr1 =  new int[]{3,8,8};
         int [] arr2 =new int[]{2,8,8,10,15};
-
+        
     }
 }
