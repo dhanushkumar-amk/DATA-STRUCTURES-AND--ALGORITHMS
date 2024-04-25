c9a47f3c2678196429c02946ccd1149185a1d843
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-25 10:20:22 +0530

    modified

--- src/SORTING/CountInversions.java
+++ src/SORTING/CountInversions.java
@@ -1,2 +1,24 @@
-package SORTING;public class CountInversions {
+package SORTING;
+
+import java.util.ArrayList;
+
+public class CountInversions {
+    public static void main(String[] args) {
+        int [] arr ={2,4,1,3,5};
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+
+        for (int i=0; i< arr.length; i++)
+        {
+            ArrayList<Integer> temp=new ArrayList<>();
+            for (int j=i+1; j< arr.length; j++)
+            {
+                if(arr[i]>arr[j]) {
+                    temp.add(arr[i]);
+                    temp.add(arr[j]);
+                }
+            }
+            list.add(temp);
+            temp.clear();
+        }
+    }
 }
