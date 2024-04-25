a56ea3e97023487b81bff8781636eb5f96657b4b
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-25 16:41:48 +0530

    modified

--- src/SORTING/SORTING.java
+++ src/SORTING/SORTING.java
@@ -1,13 +1,11 @@
+package SORTING;
 import java.util.ArrayList;
-
 class SORTING {
     static long count = 0;
-
     static void mergeSort(long[] arr, int low, int mid, int high) {
         int left = low;
         int right = mid + 1;
         ArrayList<Long> list = new ArrayList<>();
-
         while (left <= mid && right <= high) {
             if (arr[left] <= arr[right]) {
                 list.add(arr[left++]);
@@ -21,7 +19,6 @@ class SORTING {
         for (int i = low; i <= high; i++)
             arr[i] = list.get(i - low);
     }
-
     static void merge(long arr[], int l, int r) {
         if (l < r) {
             int mid = (l + r) / 2;
@@ -30,7 +27,6 @@ class SORTING {
             mergeSort(arr, l, mid, r);
         }
     }
-
     public static void main(String[] args) {
         long[] arr = {2, 3, 4, 5, 6};
         merge(arr, 0, arr.length - 1);
