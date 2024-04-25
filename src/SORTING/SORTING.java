37ea139f4575e4a1a8488f2a784097e87f938ad6
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-25 11:54:00 +0530

    modified

--- src/SORTING/SORTING.java
+++ src/SORTING/SORTING.java
@@ -39,7 +39,7 @@ import java.util.ArrayList;
 
 
         public static void main(String[] args) {
-            long [] arr = {2,4,1,3,5};
+            long [] arr = {10, 10, 10};
             merge(arr, 0,arr.length-1);
             for (long x:arr) System.out.print(x+" ");
             System.out.println();
