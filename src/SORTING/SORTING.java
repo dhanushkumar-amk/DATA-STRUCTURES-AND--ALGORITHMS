c79443df3d59c7574c5735e9c1f46304f649dc2e
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-25 11:27:15 +0530

    modified

--- src/SORTING/SORTING.java
+++ src/SORTING/SORTING.java
@@ -1,36 +1,42 @@
+package  SORTING;
+
 import java.util.ArrayList;
 
-class SORTING {
-    static void mergeSort(long[] arr, int low, int mid, int high) {
-        int left = low;
-        int right = mid + 1;
-        ArrayList<Long> list = new ArrayList<>(); // Changed from ArrayList<Integer> to ArrayList<Long>
-        while (left <= mid && right <= high) {
-            if (arr[left] <= arr[right])
-                list.add(arr[left++]);
-            else
-                list.add(arr[right++]);
-        }
-        while (left <= mid) list.add(arr[left++]);
-        while (right <= high) list.add(arr[right++]);
-        for (int i = low; i <= high; i++)
-            arr[i] = list.get(i - low); // Changed to i - low to fix Index Out of Bounds issue
-    }
 
-    static void merge(long arr[], int l, int r) {
-        if (l < r) { // Added a base case to avoid infinite recursion
-            int mid = (l + r) / 2;
-            merge(arr, l, mid);
-            merge(arr, mid + 1, r);
-            mergeSort(arr, l, mid, r); // Changed from mergeSort to mergeSort to fix the method call
-        }
-    }
+    class SORTING
+    {
+        // arr[]: Input Array
+        // N : Size of the Array arr[]
+        //Function to count inversions in the array.
+        static void mergeSort(long[] arr,int low,int mid,int high) {
+            int left=low;
+            int right=mid+1;
+            ArrayList<Integer> list = new ArrayList();
+            while(left<=mid && right <=high)
+            {
+                if(arr[left]<=arr[right])
+                    list.add((int) arr[left++]);
+                else
+                    list.add((int)arr[right++]);
+            }
+            while(left<=mid) list.add((int)arr[left++]);
+            while(right<=high)list.add((int)arr[right++]);
+            for(int i=low; i<=high; i++)
+                arr[i]=list.get(i-low);
 
-    public static void main(String[] args) {
-        long[] arr = {2, 4, 1, 3, 5};
-        merge(arr, 0, arr.length - 1); // Changed from arr.length to arr.length - 1
-        for (long num : arr) {
-            System.out.print(num + " ");
+        }
+        static void merge(long arr[],int l,int r) {
+            if(l<r)
+            {
+                int mid=(l+r)/2;
+                merge(arr,l,mid);
+                merge(arr,mid+1,r);
+                mergeSort(arr,l,mid,r);
+            }
+        }
+
+        public static void main(String[] args) {
+            long [] arr ={2,4,1,3,5};
+            merge(arr,0, arr.length);
         }
     }
-}
