1cef465cd4129aeb66b4f64a5063a987123c6124
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-26 11:21:39 +0530

    modified

--- src/BIT_WISE/countBit.java
+++ src/BIT_WISE/countBit.java
@@ -1,2 +1,20 @@
-package BIT_WISE;public class countBit {
+package BIT_WISE;
+
+public class countBit {
+    static int setBits(int N) {
+        // code here
+        int count=0;
+
+        while(N>0)
+        {
+            if((N&1)==1){
+                count++;
+            }
+            N=N>>>1;
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        setBits()
+    }
 }
