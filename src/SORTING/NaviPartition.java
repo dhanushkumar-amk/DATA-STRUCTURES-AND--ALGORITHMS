9920823dacc626add08b740d5a8f59a40dd6c05c
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-26 16:08:17 +0530

    modified

--- src/SORTING/NaviPartition.java
+++ src/SORTING/NaviPartition.java
@@ -11,7 +11,7 @@ public class NaviPartition {
             if(arr[i]<arr[p])
                 temp[k++]=arr[i];
         }
-//        arr[k]=arr[p];
+        temp[k]=arr[p];
         for (int j=0; j< arr.length;j++)
         {
             if(arr[j]>arr[p])
