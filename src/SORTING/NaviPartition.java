64d2789f4aaf34ab603d48007c4d39863073573b
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-26 16:01:59 +0530

    modified

--- src/SORTING/NaviPartition.java
+++ src/SORTING/NaviPartition.java
@@ -3,9 +3,14 @@ package SORTING;
 public class NaviPartition {
     public static void main(String[] args) {
         int [] arr ={10,9,8,7,6,5,4,3,2,1};
-        int p=5;
-        int temp=arr[p];
-        
+        int [] temp= new int[arr.length];
+        int p=4;
+        for (int i=0; i< arr.length; i++)
+        {
+            if(arr[i]<=arr[p])
+                temp[i]=arr[i];
+        }
+        for (int x:temp) System.out.print(x+" ");
     }
 
 }
