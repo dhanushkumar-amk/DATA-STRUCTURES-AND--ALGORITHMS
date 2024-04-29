30b858abf77ca60abd2f4b42700d569ba89e3eaa
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-29 19:00:45 +0530

    modified

--- src/SORTING/HoaresPartition.java
+++ src/SORTING/HoaresPartition.java
@@ -2,7 +2,7 @@ package SORTING;
 
 public class HoaresPartition {
     public static void main(String[] args) {
-        int [] arr ={27,34,43,11,15,20,28,45,27,32};
+        int [] arr ={36,34,43,11,15,20,28,45,27,32};
         int n= arr.length;
         int left=0;
         int pivot=n-1;
