23bb5b18a6c9d20f0dfde7a5863ea19846c6911a
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-29 13:06:07 +0530

    modified

--- src/SORTING/LomutoPartition.java
+++ src/SORTING/LomutoPartition.java
@@ -4,20 +4,18 @@ public class LomutoPartition {
     public static void main(String[] args) {
         int [] arr= new int[] {12,9,7,15,10};
         int n=arr.length;
-        int pivot=n-1;
+        int pivot=arr[n-1];
         int j=0;
         int i=0;
-        for (;i<n; j++)
+        for (;j<=n-1; j++)
         {
-            if(arr[j]<=arr[pivot])
+            if(arr[j]<=pivot)
             {
                 int x=arr[i];
                 arr[i]=arr[j];
                 arr[j]=x;
-                i++;
+                
             }
-            else j++;
         }
-        for (int x:arr) System.out.print(x+" ");
     }
 }
