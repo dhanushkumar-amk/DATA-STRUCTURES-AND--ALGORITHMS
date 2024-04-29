4c2356d887285e2d25dd76dd690c3ef520bf8a3a
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-29 18:06:04 +0530

    modified

--- src/SORTING/HoaresPartition.java
+++ src/SORTING/HoaresPartition.java
@@ -5,8 +5,22 @@ public class HoaresPartition {
         int [] arr ={36,34,43,11,15,20,28,45,27,32};
         int n= arr.length;
         int left=0;
-        int right=n-2;
         int pivot=n-1;
-        while(left)
+        int right=pivot-1;
+        while(left<=right)
+        {
+            if(arr[left]>arr[right])
+            {
+                int x=arr[left];
+                arr[left]=arr[right];
+                arr[right]=x;
+                left++;
+            }
+            else right++;
+        }
+        int temp=arr[left];
+        arr[left]=arr[pivot];
+        arr[pivot]=temp;
+        for (int x:arr) System.out.print(x+" ");
     }
 }
