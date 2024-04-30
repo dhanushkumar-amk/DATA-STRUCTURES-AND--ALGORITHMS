a02a120778f53b88929a0a4df82837167a41ad8d
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-30 16:59:20 +0530

    modified

--- src/SORTING/HoaresPartition.java
+++ src/SORTING/HoaresPartition.java
@@ -1,5 +1,6 @@
 package SORTING;
 
+
 public class HoaresPartition {
     public static void main(String[] args) {
         int arr[] = new int[]{9, 7, 5, 14, 2, 3, 6, 10};
@@ -8,13 +9,26 @@ public class HoaresPartition {
         for (int temp : arr)
             System.out.print(temp + " ");
     }
+    static void swap(int arr[] ,int pivot,int j) {
+        int x=arr[pivot];
+        arr[pivot]=arr[j];
+        arr[j]=x;
+    }
     static int partition(int[] arr,int low,int high) {
         int i=low;
         int j=high;
         int pivot=arr[low];
         while(i<j)
         {
-            
+            i++;
+            while(arr[i]<pivot)
+                i++;
+            while(arr[j]>pivot)
+                j--;
+            if(i>=j)return j;
+            swap(arr,i,j);
         }
+        swap(arr,low,j);
+       return 0;
     }
 }
