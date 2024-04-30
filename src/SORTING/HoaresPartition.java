b05ac9561f358c71bb50557216df38a22596b112
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-30 12:52:36 +0530

    modified

--- src/SORTING/HoaresPartition.java
+++ src/SORTING/HoaresPartition.java
@@ -2,7 +2,7 @@ package SORTING;
 
 public class HoaresPartition {
     public static void main(String[] args) {
-        int arr[] = new int[]{5, 4, 3, 2, 1};
+        int arr[] = new int[]{9,7,5,14,2,3,6,10};
 
         int n = arr.length;
         partition(arr, 0, n - 1);
