0790e1321c379ee0a93e14dd845e2fb04665c740
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-30 12:08:01 +0530

    modified

--- src/SORTING/QuickSort.java
+++ src/SORTING/QuickSort.java
@@ -2,24 +2,6 @@ package SORTING;
 
 public class QuickSort {
     public static void main(String[] args) {
-        static void partition(int arr[], int low, int high) {
-            int l=-1;
-            int r=high-1;
-            int pivot=arr[0];
-            while(l<r)
-            {
-                l++;
-                while(l<=r&& arr[l]<pivot)
-                    l++;
-                while(r>=l && arr[r]>pivot)
-                    r--;
-                if(l<=r)
-                {
-                    int x=arr[l];
-                    arr[l]=arr[r];
-                    arr[r]=x;
-                }
-            }
-        }
+
     }
 }
