42612e717006b88723a0375bbe755fcc35bdcb0e
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-30 18:34:44 +0530

    modified

--- src/SORTING/QuickSort.java
+++ src/SORTING/QuickSort.java
@@ -4,7 +4,7 @@ public class QuickSort {
     public static void main(String[] args) {
         int[] arr = {4, 1, 3, 9, 7};
         quickSort(arr, 0, arr.length - 1);
-
+        for (int x:arr) System.out.println(x+" ");
     }
     static void quickSort(int arr[], int low, int high)
     {
