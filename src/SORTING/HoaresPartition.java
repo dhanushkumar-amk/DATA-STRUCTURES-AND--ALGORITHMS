7520fa6ec9becfc2a70367eb17ab87a8feeb9ddc
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-30 11:11:09 +0530

    modified

--- src/SORTING/HoaresPartition.java
+++ src/SORTING/HoaresPartition.java
@@ -2,33 +2,10 @@ package SORTING;
 
 public class HoaresPartition {
     static void hores(int [] arr,int n) {
-        int pivot = arr[0];
-        int i = 0 - 1, j = n + 1;
-
-        while (true)
-        {
-            // Find leftmost element greater
-            // than or equal to pivot
-            do {
-                i++;
-            } while (arr[i] < pivot);
-
-            // Find rightmost element smaller
-            // than or equal to pivot
-            do {
-                j--;
-            } while (arr[j] > pivot);
-
-            // If two pointers met.
-            if (i >= j)
-                return j;
-
-            // swap(arr[i], arr[j]);
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-
-        }
+        int start=-1;
+        int end=n+1;
+        int pivot=arr[0];
+        while(true)
     }
     public static void main(String[] args) {
         int [] arr ={9,7,5,14,2,3,6,10};
