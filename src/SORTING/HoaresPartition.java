3c237371f6c1f5ffeb5e452320e0e03ee9590fa2
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-30 11:24:44 +0530

    modified

--- src/SORTING/HoaresPartition.java
+++ src/SORTING/HoaresPartition.java
@@ -1,17 +1,18 @@
 package SORTING;
 
 public class HoaresPartition {
-    static void hores(int [] arr,int n) {
+    static int[] hores(int [] arr,int n) {
         int left=0;
         int right=n-1;
-        int pivot=5;
+        int ind=0;
+        int pivot=arr[ind];
         while(true)
         {
             while(arr[left]<pivot)
                 left++;
             while(arr[right]>pivot)
                 right--;
-            if(left>=right)return;
+            if(left>=right)return new int[]{right,ind};
             int x=arr[left];
             arr[left]=arr[right];
             arr[right]=x;
@@ -21,6 +22,7 @@ public class HoaresPartition {
         int [] arr ={5,3,8,4,2,7,1,10};
         int n= arr.length;
         hores(arr,n);
+        int x=arr[ind];
         for (int x:arr) System.out.print(x+" ");
         System.out.println();
     }
