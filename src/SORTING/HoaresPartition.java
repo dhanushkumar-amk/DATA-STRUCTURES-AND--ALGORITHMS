ce51ecc97eb2e44e0d8f9a1f8eaf8a93baf36e35
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-30 13:17:02 +0530

    modified

--- src/SORTING/HoaresPartition.java
+++ src/SORTING/HoaresPartition.java
@@ -3,15 +3,15 @@ package SORTING;
 public class HoaresPartition {
     public static void main(String[] args) {
         int arr[] = new int[]{9,7,5,14,2,3,6,10};
-
         int n = arr.length;
-      int ind[] =  partition(arr, 0, n - 1);
-//      int temp=arr[ind[0]+1];
-//      arr[ind[0]+1]=arr[ind[1]];
-//      arr[ind[1]]=temp;
+        int[] indices = partition(arr, 0, n - 1);
+        int i = indices[0];
+        int j = indices[1];
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
         for (int x : arr)
             System.out.print(x + " ");
-
     }
 
     static int[] partition(int arr[], int l, int h)
