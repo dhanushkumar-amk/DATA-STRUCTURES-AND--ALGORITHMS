3282cf32edc61f9bb5122f637ddff9158635b340
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-30 12:07:23 +0530

    modified

--- src/SORTING/HoaresPartition.java
+++ src/SORTING/HoaresPartition.java
@@ -12,23 +12,5 @@ public class HoaresPartition {
 
     }
 
-    static void partition(int arr[], int low, int high) {
-        int l=-1;
-        int r=high-1;
-        int pivot=arr[0];
-        while(l<r)
-        {
-            l++;
-            while(l<=r&& arr[l]<pivot)
-                l++;
-            while(r>=l && arr[r]>pivot)
-                r--;
-            if(l<=r)
-            {
-                int x=arr[l];
-                arr[l]=arr[r];
-                arr[r]=x;
-            }
-        }
-    }
+
 }
