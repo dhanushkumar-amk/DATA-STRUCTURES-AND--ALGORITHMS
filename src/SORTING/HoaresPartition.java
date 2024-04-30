4b7da1cfb0f4dae08324380952a5a47df3576e0b
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-30 11:37:32 +0530

    modified

--- src/SORTING/HoaresPartition.java
+++ src/SORTING/HoaresPartition.java
@@ -4,7 +4,7 @@ public class HoaresPartition {
     static void hores(int [] arr,int n) {
         int left=0;
         int right=n-1;
-        int ind=0;
+        int ind=2;
         int pivot=arr[ind];
         while(true)
         {
