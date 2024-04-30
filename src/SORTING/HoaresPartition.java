d483fd816828ebbe157e0c08a8c4695fc5603bf0
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-30 11:35:55 +0530

    modified

--- src/SORTING/HoaresPartition.java
+++ src/SORTING/HoaresPartition.java
@@ -2,39 +2,26 @@ package SORTING;
 
 public class HoaresPartition {
     static void hores(int [] arr,int n) {
-        int pivot = arr[0];
-        int i = 0 - 1, j = n + 1;
-
-        while (true)
+        int left=0;
+        int right=n-1;
+        int ind=0;
+        int pivot=arr[ind];
+        while(true)
         {
-            // Find leftmost element greater
-            // than or equal to pivot
-            do {
-                i++;
-            } while (arr[i] < pivot);
-
-            // Find rightmost element smaller
-            // than or equal to pivot
-            do {
-                j--;
-            } while (arr[j] > pivot);
-
-            // If two pointers met.
-            if (i >= j)
-                return ;
-
-            // swap(arr[i], arr[j]);
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-
+            while(arr[left]<pivot)
+                left++;
+            while(arr[right]>pivot)
+                right--;
+            if(left>=right)return ;
+            int x=arr[left];
+            arr[left]=arr[right];
+            arr[right]=x;
         }
     }
     public static void main(String[] args) {
-        int [] arr ={10, 17, 18, 9, 11, 15};
-        int n= arr.length-1;
-     hores(arr,n);
-
+        int [] arr ={5,3,8,4,2,7,1,10};
+        int n= arr.length;
+        hores(arr,n);
         for (int t:arr) System.out.print(t+" ");
         System.out.println();
     }
