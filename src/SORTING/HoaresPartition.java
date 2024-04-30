b62edfd938f776d9deaa10391f2b0cf20de24b33
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-04-30 13:25:13 +0530

    modified

--- src/SORTING/HoaresPartition.java
+++ src/SORTING/HoaresPartition.java
@@ -4,29 +4,13 @@ public class HoaresPartition {
     public static void main(String[] args) {
         int arr[] = new int[]{9,7,5,14,2,3,6,10};
         int n = arr.length;
-        int[] indices = partition(arr, 0, n - 1);
-        int x=arr[indices[0]+1];
-        arr[indices[0]+1]=arr[indices[1]+1];
-        arr[indices[0]+1]=x;
+        partition(arr, 0, n - 1);
         for (int temp : arr)
             System.out.print(temp + " ");
     }
 
-    static int[] partition(int arr[], int l, int h)
+    static void  partition(int arr[], int l, int h)
     {
-        int pivot=arr[l];
-        int i=l-1,j=h+1;
-        while(true){
-            do{
-                i++;
-            }while(arr[i]<pivot);
-            do{
-                j--;
-            }while(arr[j]>pivot);
-            if(i>=j)return new int[]{i,j};
-            int temp=arr[i];
-            arr[i]=arr[j];
-            arr[j]=temp;
-        }
+        
     }
 }
