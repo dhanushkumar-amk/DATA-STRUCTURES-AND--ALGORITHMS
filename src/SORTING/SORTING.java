b85755512757cbfddc72118d62ecd0e8c8e3f6ab
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-05-01 18:53:18 +0530

    modified

--- src/SORTING/SORTING.java
+++ src/SORTING/SORTING.java
@@ -1,37 +1,17 @@
-package SORTING;
-import java.util.ArrayList;
-class SORTING {
-    static long count = 0;
-    static void mergeSort(long[] arr, int low, int mid, int high) {
-        int left = low;
-        int right = mid + 1;
-        ArrayList<Long> list = new ArrayList<>();
-        while (left <= mid && right <= high) {
-            if (arr[left] <= arr[right]) {
-                list.add(arr[left++]);
-            } else {
-                list.add(arr[right++]);
-                count += mid - left + 1; // Increment count by the number of remaining elements in left half
-            }
+package  SORTING;
+
+import java.util.Arrays;
+
+class Solution{
+    //Function to find minimum difference between any pair of elements in an array.
+    public static int MinimumDifference(int arr[], int n) {
+        Arrays.sort(arr);
+        int Min=Integer.MIN_VALUE;
+        for(int i=0; i<n; i++)
+        {
+            if(i>0 && arr[i]-arr[i-1]<Min)
+                Min=Math.min(arr[i]-arr[i-1],min);
         }
-        while (left <= mid) list.add(arr[left++]);
-        while (right <= high) list.add(arr[right++]);
-        for (int i = low; i <= high; i++)
-            arr[i] = list.get(i - low);
-    }
-    static void merge(long arr[], int l, int r) {
-        if (l < r) {
-            int mid = (l + r) / 2;
-            merge(arr, l, mid);
-            merge(arr, mid + 1, r);
-            mergeSort(arr, l, mid, r);
-        }
-    }
-    public static void main(String[] args) {
-        long[] arr = {2, 3, 4, 5, 6};
-        merge(arr, 0, arr.length - 1);
-        for (long x : arr) System.out.print(x + " ");
-        System.out.println();
-        System.out.println(count);
+        return Min;
     }
 }
