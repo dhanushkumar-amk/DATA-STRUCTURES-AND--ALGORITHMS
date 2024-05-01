7e1fd49d6446dd368a03069b7c8bfe349a907fd7
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-05-01 10:45:55 +0530

    modified

--- src/SORTING/ThreeWayPartition.java
+++ src/SORTING/ThreeWayPartition.java
@@ -1,29 +1,35 @@
 package SORTING;
 
 public class ThreeWayPartition {
-    static int partition(int[]arr,int x) {
-        int l=0;
-        int r=arr.length-1;
-        int pivot=x;
-        while(l<r)
-        {
-            l++;
-            while(arr[l]<pivot)
-                l++;
-            while(arr[r]>pivot)
-                r--;
-            if(l>r)return 0;
-            int temp=arr[l];
-            arr[l]=arr[r];
-            arr[r]=temp;
+    static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high]; // Selecting the last element as the pivot
+        int i = low - 1; // Index of smaller element
+
+        for (int j = low; j < high; j++) {
+            // If current element is smaller than or equal to the pivot
+            if (arr[j] <= pivot) {
+                i++;
+
+                // Swap arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
         }
-        return -1;
+
+        // Swap arr[i+1] and arr[high] (or pivot)
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
     }
     public static void main(String[] args) {
         int [] arr ={10 ,7, 6 ,1 ,4, 10 ,5 ,2 ,7, 5, 3, 3, 8, 3 ,8};
         int a=5;
         int b=5;
-        partition(arr,a);
+        partition(arr,0,a);
+        partition(arr,0,b);
         for (int x:arr) System.out.print(x+" ");
     }
 }
