5b2ee20879ab8a27f8e74e5dd6c030c36886ca7a
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-05-01 10:28:13 +0530

    modified

--- src/SORTING/ThreeWayPartition.java
+++ src/SORTING/ThreeWayPartition.java
@@ -1,2 +1,26 @@
-package SORTING;public class ThreeWayPartition {
+package SORTING;
+
+public class ThreeWayPartition {
+    static void partition(int[]arr,int x) {
+        int left=-1;
+        int right=arr.length;
+        while(true)
+        {
+            do{
+                left++;
+            }while(arr[left]<x);
+            do{
+                right--;
+            }while(arr[right]>x);
+            if(left>=right)return ;
+            int temp=arr[left];
+            arr[left]=arr[right];
+            arr[right]=temp;
+        }
+    }
+    public static void main(String[] args) {
+        int [] arr ={5,4,3,2,1};
+        int a=
+        partition();
+    }
 }
