317ae6726390b9d3ecb30174f7682a6ad56dbf79
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-05-04 22:56:52 +0530

    modified

--- src/SORTING/ChocolateDistributionProblem.java
+++ src/SORTING/ChocolateDistributionProblem.java
@@ -11,7 +11,7 @@ public class ChocolateDistributionProblem {
         int right=m-1;
         int max=Integer.MIN_VALUE;
         System.out.println(arr[m-1]);
-        System.out.println(arr[m[0]]);
+        System.out.println(arr[0]);
         System.out.println(arr[m-1]-arr[0]);
     }
 }
