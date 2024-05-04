e452269bf999d52eeaac6cad610805640cd86fbc
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-05-04 18:05:01 +0530

    modified

--- src/SORTING/ChocolateDistributionProblem.java
+++ src/SORTING/ChocolateDistributionProblem.java
@@ -1,2 +1,20 @@
-package SORTING;public class ChocolateDistributionProblem {
+package SORTING;
+
+import java.util.Arrays;
+
+public class ChocolateDistributionProblem {
+    public static void main(String[] args) {
+        int [] arr={7,3,2,4,9,12,56};
+        Arrays.sort(arr);
+        int m=3;
+        int left=0;
+        int right=3;
+        int min=Integer.MAX_VALUE;
+        while(left<right)
+        {
+            if(arr[right]-arr[left]<min)
+                min=Math.min(min,arr[right]-arr[left]);
+        }
+        
+    }
 }
