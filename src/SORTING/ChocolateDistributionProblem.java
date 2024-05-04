68ffe1585901e1647bf1627aff5aa5801e5a4560
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-05-04 18:11:42 +0530

    modified

--- src/SORTING/ChocolateDistributionProblem.java
+++ src/SORTING/ChocolateDistributionProblem.java
@@ -10,6 +10,7 @@ public class ChocolateDistributionProblem {
         int left=0;
         int right=m-1;
         int max=Integer.MIN_VALUE;
+        for (int x:arr) System.out.print(x+" ");
         while(left<=right)
         {
             if(arr[right]-arr[left]>max)
