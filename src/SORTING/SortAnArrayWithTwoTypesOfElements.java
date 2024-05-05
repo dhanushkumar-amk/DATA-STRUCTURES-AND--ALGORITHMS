c1332dd236c724f2e6f79be4d06011781c5305a9
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-05-05 15:10:08 +0530

    modified

--- src/SORTING/SortAnArrayWithTwoTypesOfElements.java
+++ src/SORTING/SortAnArrayWithTwoTypesOfElements.java
@@ -9,13 +9,12 @@ public class SortAnArrayWithTwoTypesOfElements {
         for (int x:arr)
             System.out.print(x+" ");
         int [] arr2 ={15,14,13,12};
-        Arrays.sort(arr2);
         // 12 13 14 15
         // e   o  e  o
-        // 
+        //
         for (int i=0; i< arr2.length; i++)
         {
-
+            if((arr[i]&1)==1)
         }
     }
 }
