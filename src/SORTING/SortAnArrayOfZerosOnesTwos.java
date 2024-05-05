0c350ad2d4d2b59e178bfa0186d7d092bc5babb6
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-05-05 17:44:03 +0530

    modified

--- src/SORTING/SortAnArrayOfZerosOnesTwos.java
+++ src/SORTING/SortAnArrayOfZerosOnesTwos.java
@@ -4,29 +4,27 @@ import java.util.Arrays;
 
 public class SortAnArrayOfZerosOnesTwos {
     public static void main(String[] args) {
-        int[] arr = {0, 1, 2, 1, 2, 0};
-        int start = 0;
-        int n = arr.length;
-        int a = 0;
-        int b = 1;
-        int c = 2;
-        int end = n - 1;
-        int j = 0;
-        while (j <= end) {
-            if (arr[j] < b) {
+        int [] arr ={0,1,2,1,2,0};
+        int start=0;
+        int n= arr.length;;
+        int a=0;
+        int b=1;
+        int c=2;
+        int end=n-1;
+        int j=0;
+        while(j<=end) {
+            if (arr[j] <=a) {
                 int x = arr[start];
                 arr[start] = arr[j];
                 arr[j] = x;
                 start++;
                 j++;
-            } else if (arr[j] > b) {
+            } else if (arr[j] >= c) {
                 int x = arr[j];
                 arr[j] = arr[end];
                 arr[end] = x;
                 end--;
-            } else {
-                j++;
-            }
+            } else j++;
         }
         System.out.println(Arrays.toString(arr));
     }
