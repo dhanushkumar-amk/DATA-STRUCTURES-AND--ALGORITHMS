dd238448452164ede8bda2b8945e1ff1396a2393
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-05-05 15:34:37 +0530

    modified

--- src/SORTING/SortAnArrayWithTwoTypesOfElements.java
+++ src/SORTING/SortAnArrayWithTwoTypesOfElements.java
@@ -1,11 +1,8 @@
 package SORTING;
 
-import java.util.Arrays;
-
 public class SortAnArrayWithTwoTypesOfElements {
     public static void main(String[] args) {
         int [] arr ={15,-3,-2,18};
-        Arrays.sort(arr);
         for (int x:arr)
             System.out.print(x+" ");
         int [] arr2 ={15,14,13,12};
