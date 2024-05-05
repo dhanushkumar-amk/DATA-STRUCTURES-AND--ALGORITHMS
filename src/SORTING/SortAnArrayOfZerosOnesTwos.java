1675c278f1a8c3e5c292aa595bb3561694d4264b
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-05-05 16:50:23 +0530

    modified

--- src/SORTING/SortAnArrayOfZerosOnesTwos.java
+++ src/SORTING/SortAnArrayOfZerosOnesTwos.java
@@ -2,6 +2,8 @@ package SORTING;
 
 public class SortAnArrayOfZerosOnesTwos {
     public static void main(String[] args) {
-        
+        int [] arr ={0,1,1,0,1,0,1};
+        int left=0;
+        int right=arr.length;
     }
 }
