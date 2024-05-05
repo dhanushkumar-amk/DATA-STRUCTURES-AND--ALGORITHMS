fca1d6a728f1d05f023a83308940f1f17ee7a9b2
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-05-05 15:38:01 +0530

    modified

--- src/SORTING/SortAnArrayWithTwoTypesOfElements.java
+++ src/SORTING/SortAnArrayWithTwoTypesOfElements.java
@@ -3,13 +3,23 @@ package SORTING;
 public class SortAnArrayWithTwoTypesOfElements {
     public static void main(String[] args) {
         int [] arr ={15,-3,-2,18};
-        int lef=0;
+        int lef=-1;
         int rig= arr.length-1;
         while(lef<rig)
         {
-            
+            lef++;
+            while(arr[lef]<0)
+                lef++;
+            while(arr[rig]>0)
+                rig--;
+            if(lef<rig)
+            {
+                int x=arr[lef];
+                arr[lef]=arr[rig];
+                arr[rig]=x;
+            }
         }
-
+        for (int x:arr) System.out.print(x+" ");
         for (int x:arr)
             System.out.print(x+" ");
         int [] arr2 ={15,14,13,12};
