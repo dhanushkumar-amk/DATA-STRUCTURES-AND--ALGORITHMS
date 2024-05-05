8b863c6c9b67fed7ee51688d365464c292d11326
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-05-05 16:52:51 +0530

    modified

--- src/SORTING/SortAnArrayOfZerosOnesTwos.java
+++ src/SORTING/SortAnArrayOfZerosOnesTwos.java
@@ -4,6 +4,17 @@ public class SortAnArrayOfZerosOnesTwos {
     public static void main(String[] args) {
         int [] arr ={0,1,1,0,1,0,1};
         int left=0;
-        int right=arr.length;
+        int right=arr.length-1;
+        int pivot=1;
+        while(left<right) {
+            while (left < right && arr[left] < pivot)
+                left++;
+            while(left< right && arr[right]>pivot)
+                right--;
+            if(left<right)
+            {
+                
+            }
+        }
     }
 }
