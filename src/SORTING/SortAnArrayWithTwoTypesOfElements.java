95e5e7f5f463e7e23af841dfe9326c52d3cf497c
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-05-05 15:25:28 +0530

    modified

--- src/SORTING/SortAnArrayWithTwoTypesOfElements.java
+++ src/SORTING/SortAnArrayWithTwoTypesOfElements.java
@@ -27,21 +27,21 @@ public class SortAnArrayWithTwoTypesOfElements {
         }
         System.out.println();
         for (int x:arr2) System.out.print(x+" ");
-        int [] binaryArr = {0,1,0,1,1,1,1,0,0,0};
-        int l=0;
-        int r= binaryArr.length-1;
-        while(l<r){
-            while(arr[l]==0 )
-                l++;
-            while(arr[r]==1)
-                r--;
-            if(l<r)
-            {
-                int x=arr[l];
-                arr[l]=arr[r];
-                arr[r]=x;
-            }
-        }
-        for (int x:binaryArr) System.out.print(x+" ");
+//        int [] binaryArr = {0,1,0,1,1,1,1,0,0,0};
+//        int l=0;
+//        int r= binaryArr.length-1;
+//        while(l<r){
+//            while(arr[l]==0 )
+//                l++;
+//            while(arr[r]==1)
+//                r--;
+//            if(l<r)
+//            {
+//                int x=arr[l];
+//                arr[l]=arr[r];
+//                arr[r]=x;
+//            }
+//        }
+//        for (int x:binaryArr) System.out.print(x+" ");
     }
 }
