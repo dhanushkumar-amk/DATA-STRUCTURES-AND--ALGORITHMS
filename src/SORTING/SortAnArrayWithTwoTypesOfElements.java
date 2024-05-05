6520fcd8660f30af992a003fe9d2f637a3f89d28
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-05-05 16:13:26 +0530

    modified

--- src/SORTING/SortAnArrayWithTwoTypesOfElements.java
+++ src/SORTING/SortAnArrayWithTwoTypesOfElements.java
@@ -20,25 +20,6 @@ public class SortAnArrayWithTwoTypesOfElements {
             }
         }
         for (int x:arr) System.out.print(x+" ");
-        int [] arr2 ={15,14,13,12};
-        int n2= arr2.length;
-        int left=0;
-        int right=n2-1;
-        while(left<right)
-        {
-            while((arr2[left]%2)==0)
-                left++;
-            while(((arr2[right]%2)==1))
-                right--;
-            if(left<right)
-            {
-                int x=arr2[left];
-                arr2[left]=arr2[right];
-                arr2[right]=x;
-            }
-        }
-        System.out.println();
-        for (int x:arr2) System.out.print(x+" ");
 
     }
 }
