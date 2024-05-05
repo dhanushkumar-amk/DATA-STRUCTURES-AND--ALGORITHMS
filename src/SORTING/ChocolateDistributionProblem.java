7cfbbf34d0669807a3e9afd7ece8a8e86b53397a
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-05-05 11:28:04 +0530

    modified

--- src/SORTING/ChocolateDistributionProblem.java
+++ src/SORTING/ChocolateDistributionProblem.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class ChocolateDistributionProblem {
     public static void main(String[] args) {
-        int [] arr = {7, 3, 2, 4, 9, 12, 56};
+        int [] arr = {3, 4, 1, 9, 56, 7, 9, 12};
         Arrays.sort(arr);
         int m=3;
         int left=0;
