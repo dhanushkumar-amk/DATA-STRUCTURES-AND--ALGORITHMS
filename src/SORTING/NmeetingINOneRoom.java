f7893425894c9b93ba792349399214d3b02dcb1e
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-05-07 21:30:20 +0530

    modified

--- src/SORTING/NmeetingINOneRoom.java
+++ src/SORTING/NmeetingINOneRoom.java
@@ -1,10 +1,13 @@
 package SORTING;
 public  class NmeetingINOneRoom  {
-    static int maxMeetings() {
+    static int maxMeetings(int [] start ,int [] end ,int n) {
 
     }
     public static void main(String[] args) {
         int [] start=  {1,3,0,5,8,5};
-        int [] end  =  {}
+        int [] end  = {2,4,6,7,9,9};
+        int n=start.length;
+       int res= maxMeetings(start,end,n);
+        System.out.println(res);
     }
 }
