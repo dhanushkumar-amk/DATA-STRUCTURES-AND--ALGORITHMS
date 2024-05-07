66093177e99d3d2068e1b6376b1efbcb477a0aeb
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-05-07 14:16:47 +0530

    modified

--- src/SORTING/MergeOverlappingIntervals.java
+++ src/SORTING/MergeOverlappingIntervals.java
@@ -1,11 +1,9 @@
 package SORTING;
 
-import java.util.Arrays;
-
 public class MergeOverlappingIntervals {
     public static void main(String[] args) {
         int[][] arr = {{2, 6}, {8, 10}, {2, 3}, {15, 18}};
-        Arrays.sort(arr);
+//        Arrays.sort(arr);
         for (int i = 0; i < arr.length; i++) {
             for (int j=i; j< arr.length; j++)
             {
