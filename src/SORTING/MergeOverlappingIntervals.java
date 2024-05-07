cb2949ed07c919686e96ed0d558b443175d96ee2
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-05-07 14:17:23 +0530

    modified

--- src/SORTING/MergeOverlappingIntervals.java
+++ src/SORTING/MergeOverlappingIntervals.java
@@ -5,7 +5,7 @@ public class MergeOverlappingIntervals {
         int[][] arr = {{2, 6}, {8, 10}, {2, 3}, {15, 18}};
 //        Arrays.sort(arr);
         for (int i = 0; i < arr.length; i++) {
-            for (int j=i; j< arr.length; j++)
+            for (int j=i; j<arr[i].length; j++)
             {
                 System.out.print(arr[j]+" ");
             }
