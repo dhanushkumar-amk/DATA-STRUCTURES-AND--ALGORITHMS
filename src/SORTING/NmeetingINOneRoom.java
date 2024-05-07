2d424095de42a437437e64134f0956e2be6f1abf
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-05-07 21:09:51 +0530

    modified

--- src/SORTING/NmeetingINOneRoom.java
+++ src/SORTING/NmeetingINOneRoom.java
@@ -1,2 +1,25 @@
-package SORTING;public class NmeetingINOneRoom {
+package SORTING;
+
+public class NmeetingINOneRoom {
+    public static int maxMeetings(int start[], int end[], int n)
+    {
+        // add your code here
+        Arrays.sort(end);
+        int count=1;
+        int ends=end[0];
+        for(int i=1;i<n; i++)
+        {
+            if(ends<start[i])
+            {
+                count++;
+                ends=end[i];
+            }
+            else continue;
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        int  [] 
+     maxMeetings();
+    }
 }
