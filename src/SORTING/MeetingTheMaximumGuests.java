b30d983f70361b4967aa233ffc4f00803604c043
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-05-07 21:52:01 +0530

    modified

--- src/SORTING/MeetingTheMaximumGuests.java
+++ src/SORTING/MeetingTheMaximumGuests.java
@@ -2,6 +2,8 @@ package SORTING;
 
 public class MeetingTheMaximumGuests {
     public static void main(String[] args) {
-        
+        int arr[] = { 900, 600, 700};
+        int dep[] = { 1000, 800, 730};
+        int n= arr.length;
     }
 }
