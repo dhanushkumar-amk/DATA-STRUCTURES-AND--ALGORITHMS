b2779fe697d4fbb40ec98449c97ef78a8b0fd1f5
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-05-08 21:18:23 +0530

    modified

--- src/SORTING/CyclicSort.java
+++ src/SORTING/CyclicSort.java
@@ -3,7 +3,15 @@ package SORTING;
 public class CyclicSort {
     public static void main(String[] args) throws Exception {
         int [] arr ={40,30,20,10};
-        
+        int n= arr.length;
+        for (int i=0; i<n; i++)
+        {
+            int ele=arr[i];
+            for (int j=0; j<n;j++)
+            {
+
+            }
+        }
 
     }
 }
