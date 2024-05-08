844574f2eaaff6f442933bb8403344287d696743
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-05-08 16:29:06 +0530

    modified

--- src/SORTING/MinimumPlatforms.java
+++ src/SORTING/MinimumPlatforms.java
@@ -1,5 +1,34 @@
 package SORTING;
 
+import java.util.Arrays;
+
 public class MinimumPlatforms {
-    
+    static int findPlatform(int start[], int stop[], int n)
+    {
+        // add your code here
+        Arrays.sort(start);
+        Arrays.sort(stop);
+        int j=0;
+        int i=1;
+        int res=1;
+        int count=1;
+        while(i<n && j<n)
+        {
+            if(start[i]<=stop[j])
+            {
+                i++;
+                count++;
+            }
+            else {
+                count--;
+                j++;
+            }
+            res=Math.max(count,res);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        
+    }
 }
