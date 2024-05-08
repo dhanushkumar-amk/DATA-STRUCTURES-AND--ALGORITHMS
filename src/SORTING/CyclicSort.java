1434b664e69eb84e4a666544198088de175752f4
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-05-08 17:02:19 +0530

    modified

--- src/SORTING/CyclicSort.java
+++ src/SORTING/CyclicSort.java
@@ -3,13 +3,13 @@ package SORTING;
 import java.util.Arrays;
 
 public class CyclicSort {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         int [] arr ={5,4,3,2,1};
         int i=0;
         int n= arr.length;
         while(i<n) {
             if(arr[i]>n)
-                throw 
+                throw new Exception("person");
             if(arr[i]!=i+1)
             {
                 int temp=arr[i];
