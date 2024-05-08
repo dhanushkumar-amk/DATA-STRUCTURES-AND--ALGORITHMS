1e0f81aa4918b59051829df1a3a4b0503358f7fb
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-05-08 16:41:34 +0530

    modified

--- src/SORTING/CyclicSort.java
+++ src/SORTING/CyclicSort.java
@@ -1,8 +1,21 @@
 package SORTING;
 
+import java.util.Arrays;
+
 public class CyclicSort {
     public static void main(String[] args) {
         int [] arr ={5,4,3,2,1};
-        
+        int i=0;
+        int n= arr.length;
+        while(i<n)
+        {
+            if(arr[i]!=i+1){
+                int x=arr[i];
+                arr[i]=arr[arr[i]-1];
+                arr[arr[i]-1]=x;
+            }
+            else i++;
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
