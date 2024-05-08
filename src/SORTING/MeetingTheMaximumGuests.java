26e96e0395191b525fa3d3cf3d37c738223a6c90
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-05-08 14:11:12 +0530

    modified

--- src/SORTING/MeetingTheMaximumGuests.java
+++ src/SORTING/MeetingTheMaximumGuests.java
@@ -1,11 +1,10 @@
 package SORTING;
 public class MeetingTheMaximumGuests {
-    static int  MeetingMax(int [] start,int []end ,int n){
-        
+  static int   MeetingMax(int [] start,int []end ,int n){
+
     }
     public static void main(String[] args) {
         int [] start = {900,700,900};
-        int [] end   = {1000,800,730};
-        int ans= MeetingMax(start,end,start.length);
+        int [] end   = {1000,800,}
     }
 }
