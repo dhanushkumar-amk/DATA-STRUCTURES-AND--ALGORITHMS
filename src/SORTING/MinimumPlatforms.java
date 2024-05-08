f1b3aaead3a8ad8ee5aa61dbc09082bb2ee30759
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-05-08 16:30:06 +0530

    modified

--- src/SORTING/MinimumPlatforms.java
+++ src/SORTING/MinimumPlatforms.java
@@ -30,8 +30,8 @@ public class MinimumPlatforms {
 
     public static void main(String[] args) {
 
-        int arr[] = {0900, 0940, 0950, 1100, 1500, 1800};
-        int  dep[] = {0910, 1200, 1120, 1130, 1900, 2000};
+        int arr[] = {900, 940,950, 1100, 1500, 1800};
+        int  dep[] = {910, 1200, 1120, 1130, 1900, 2000};
        int res= findPlatform(arr,dep, arr.length);
         System.out.println(res);
 
