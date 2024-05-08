6a9c67f70981f3dad0624cb95ffae09fd0eab89d
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-05-08 14:14:42 +0530

    modified

--- src/SORTING/MeetingTheMaximumGuests.java
+++ src/SORTING/MeetingTheMaximumGuests.java
@@ -6,7 +6,12 @@ public class MeetingTheMaximumGuests {
   static int   MeetingMax(int [] start,int []end ,int n){
       Arrays.sort(start);
       Arrays.sort(end);
-      
+      int i=1;
+      int j=0;
+      int count=1;
+      while(i<n&&j<n){
+          if(start[])
+      }
     }
     public static void main(String[] args) {
         int [] start = {900,700,900};
