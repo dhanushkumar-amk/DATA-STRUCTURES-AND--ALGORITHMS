c8877bddbe87f0ec423d8117e78f16d01fb849f3
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-05-08 22:33:00 +0530

    modified

--- src/SORTING/CyclicSort.java
+++ src/SORTING/CyclicSort.java
@@ -1,56 +1,26 @@
 package SORTING;
 
-class Solution
-{
-    public static void main (String[] args)
-    {
-        int arr[] = { 532,8,3,234,5};
-        int n = arr.length;
-        cycleSort(arr, n);
+import java.util.Arrays;
 
-        for (int i = 0; i < n; i++)
-            System.out.print( arr[i] + " ");
-
-    }
-
-    static void cycleSort(int arr[], int n) {
-        for (int cs = 0; cs < n - 1; cs++) {
-            int item = arr[cs];
-            int pos = cs;
-            for (int i = cs + 1; i < n; i++)
-                if (arr[i] < item)
-                    pos++;
-
-            if (pos == cs) // Skip if the current element is already at its correct position
-                continue;
-
-            // Handle duplicate elements
-            while (item == arr[pos])
-                pos++;
-
-            if (pos != cs) {
-                int temp = item;
-                item = arr[pos];
-                arr[pos] = temp;
+public class CyclicSort {
+    public static void main(String[] args) throws Exception {
+        int [] arr ={532,8,3,234,532,8};
+        int n= arr.length;
+        for (int i=0; i<n; i++)
+        {
+            int ele=arr[i];
+            int count=0;
+            for (int j=0; j<n;j++)
+            {
+                if(arr[j]<ele)
+                    count++;
             }
-
-            while (pos != cs) {
-                pos = cs;
-                for (int i = cs + 1; i < n; i++)
-                    if (arr[i] < item)
-                        pos++;
-
-                while (item == arr[pos])
-                    pos++;
-
-                if (item != arr[pos]) {
-                    int temp = item;
-                    item = arr[pos];
-                    arr[pos] = temp;
-                }
-            }
+            int x=arr[i];
+            arr[i]=arr[count];
+            arr[count]=x;
+//            System.out.println(arr[i]+" "+count);
+//            count=0;
         }
+        System.out.println(Arrays.toString(arr));
     }
-
 }
-
