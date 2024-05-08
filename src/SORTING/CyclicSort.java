61bccf6f1cb454f58f4862f8e157559135233b41
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-05-08 22:31:09 +0530

    modified

--- src/SORTING/CyclicSort.java
+++ src/SORTING/CyclicSort.java
@@ -2,25 +2,44 @@ package SORTING;
 
 import java.util.Arrays;
 
-public class CyclicSort {
-    public static void main(String[] args) throws Exception {
-        int [] arr ={532,8,3,234,532,8};
-        int n= arr.length;
-        for (int i=0; i<n; i++)
-        {
-            int ele=arr[i];
-            int count=0;
-            for (int j=0; j<n;j++)
-            {
-                if(arr[j]<ele)
-                    count++;
+class Solution
+{
+    public static void main (String[] args)
+    {
+        int arr[] = { 532,8,3,234,532,8};
+        int n = arr.length;
+        cycleSortDistinct(arr, n);
+
+        for (int i = 0; i < n; i++)
+            System.out.print( arr[i] + " ");
+
+    }
+
+    static void cycleSortDistinct(int arr[], int n)
+    {
+        for(int cs=0;cs<n-1;cs++){
+            int item=arr[cs];
+            int pos=cs;
+            for(int i=cs+1;i<n;i++)
+                if(arr[i]<item)
+                    pos++;
+            //swap(item,arr[pos])
+
+            int temp=item;
+            item=arr[pos];
+            arr[pos]=temp;
+            while(pos!=cs){
+                pos=cs;
+                for(int i=cs+1;i<n;i++)
+                    if(arr[i]<item)
+                        pos++;
+
+                //swap(item,arr[pos])
+                temp=item;
+                item=arr[pos];
+                arr[pos]=temp;
             }
-            int x=arr[i];
-            arr[i]=arr[count];
-            arr[count]=x;
-//            System.out.println(arr[i]+" "+count);
-//            count=0;
         }
-        System.out.println(Arrays.toString(arr));
     }
 }
+
