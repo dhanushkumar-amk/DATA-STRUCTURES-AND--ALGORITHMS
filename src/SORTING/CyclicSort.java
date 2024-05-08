5b69393b14024dbe3017367606980e2f683222a7
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-05-08 22:30:16 +0530

    modified

--- src/SORTING/CyclicSort.java
+++ src/SORTING/CyclicSort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class CyclicSort {
     public static void main(String[] args) throws Exception {
-        int [] arr ={532,8,3,234,532};
+        int [] arr ={532,8,3,234,532,8};
         int n= arr.length;
         for (int i=0; i<n; i++)
         {
